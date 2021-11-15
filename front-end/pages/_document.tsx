1897e0945ebd2d0bc16423a6067a9bbf482069e4
yongwookLee <yongw00k@g.skku.edu>
2021-11-16 04:07:13 +0900

    chore: Add div for modals

--- front-end/pages/_document.tsx
+++ front-end/pages/_document.tsx
@@ -34,6 +34,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id="modal-root"></div>
         </body>
       </html>
      );
