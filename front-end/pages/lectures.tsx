043f2d8279b4daefd1f83775dd522107da79ce19
yongwookLee <yongw00k@g.skku.edu>
2021-11-16 04:30:24 +0900

    feat: Add pages except styles

---
+++ front-end/pages/lectures.tsx
@@ -0,0 +1,11 @@
+import Layout from '@components/Layout';
+
+const LecturesPage = () => (
+  <Layout>
+    <div>
+      <h2>강의 페이지 입니다.</h2>
+    </div>
+  </Layout>
+)
+
+export default LecturesPage;
