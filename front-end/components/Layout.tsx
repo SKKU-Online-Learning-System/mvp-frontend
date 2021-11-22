040a2c11b9060e409d0778e78a7730d72c795505
Yongwook Lee <yongw00k@g.skku.edu>
2021-11-22 13:01:53 +0900

    feat: Set Layout grid with sass. Breakpoint is 1650px

--- front-end/components/Layout.tsx
+++ front-end/components/Layout.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Header from './Header';
+import style from './Layout.module.scss';
 
 interface LayoutProps{
   children?: React.ReactNode;
 }
 
 const Layout = ({children} : LayoutProps) => (
-  <div>
+  <div className={style.layout}>
     <Header/>
     {children}
   </div>
