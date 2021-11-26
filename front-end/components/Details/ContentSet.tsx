b9a1356f1291c47952d1a5552db18afefa854a8e
2je0 <bulljy@naver.com>
2021-11-26 18:13:04 +0900

    Add Details page and add image needed to Details Page

---
+++ front-end/components/Details/ContentSet.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+import TopContent from '@components/Details/TopContent'
+import Curriculum from '@components/Details/Curriculum'
+const ContentSet = () => {
+    return (
+        <div>
+            <TopContent></TopContent>
+            <Curriculum></Curriculum>
+        </div>
+
+  );
+}
+
+
+
+export default ContentSet;
