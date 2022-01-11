f9506781966a0d8434d9d07e241c5c8dc08863bc
HKM <rbals0445@naver.com>
2022-01-11 23:28:01 +0900

    1. 수정사항 index.tsx - 새로운 강의를 반환하는 함수 추가 shared/utils/getDiffDays ( 강의   출시가 30일 이하면 신규 강의) - beginner를 구분하기 위해 난이도가 1이면 basicLecture로 추가되게 함. - 강의 이어보기는 회원기능이 되어야 가능함.

--- front-end/components/Home/Card.tsx
+++ front-end/components/Home/Card.tsx
@@ -1,14 +1,14 @@
 interface CardProps {
-  title: string;
-  author: string;
+	title: string;
+	author: string;
 }
 
-const Card = ({title, author}: CardProps) => (
-  <div style = {{width: '22%'}} >
-    <img style = {{width: '100%'}} src='images/card_img.png'/>
-    <b>{title}</b>
-    <p>{author}</p>
-  </div>
+const Card = ({ title, author }: CardProps) => (
+	<div style={{ width: '22%' }}>
+		<img style={{ width: '100%' }} src="images/card_img.png" />
+		<b>{title}</b>
+		<p>{author}</p>
+	</div>
 );
 
 export default Card;
