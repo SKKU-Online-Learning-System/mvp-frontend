7a0b1ea9c73907296667a037457d68e6c17717c5
2je0 <82891332+2je0@users.noreply.github.com>
2022-01-26 19:43:21 +0900

    practice

--- front-end/components/Details/Banner.tsx
+++ front-end/components/Details/Banner.tsx
@@ -1,28 +1,26 @@
-import styled from 'styled-components'
-import BannerDescription from '@components/Details/BannerDescription'
-const Banner = () => {
-  return (
-    <BannerLayout>
-      <BannerImg></BannerImg>
-      <BannerDescription></BannerDescription>
+import styled from 'styled-components';
+import BannerDescription from '@components/Details/BannerDescription';
 
-    </BannerLayout>
-  );
-}
+const Banner = () => {
+	return (
+		<BannerLayout>
+			<BannerImg></BannerImg>
+			<BannerDescription></BannerDescription>
+		</BannerLayout>
+	);
+};
 const BannerLayout = styled.div`
-  background-color: #333333;
-  height: 400px;
-  width:100vw;
-  display: flex;
-  align-items: center;
+	background-color: #333333;
+	height: 400px;
+	width: 100vw;
+	display: flex;
+	align-items: center;
 `;
 const BannerImg = styled.div`
-  background-color: #000000;
-  width: 515px;
-  height: 301px;
-  margin-left: 171px;
+	background-color: #000000;
+	width: 515px;
+	height: 301px;
+	margin-left: 171px;
 `;
-
-
 
 export default Banner;
--- front-end/components/Details/BannerDescription.tsx
+++ front-end/components/Details/BannerDescription.tsx
@@ -1,59 +1,85 @@
-import styled from 'styled-components'
-import Tag from '@components/Details/Tag'
+import styled from 'styled-components';
+import Tag from '@components/Details/Tag';
+import { BASE_URL } from 'shared/constants/constant';
+import { useEffect } from 'react';
+import axios from 'axios';
+
 const BannerDescription = () => {
-  return (
-    <DescContainer>
-      <DescCategory>개발 프로그래밍 {'>'} 웹 개발</DescCategory>
-      <Margin></Margin>
-      <DescSubject>실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발</DescSubject>
-      <Margin></Margin>
-      <DescName>
-        <img style={{ marginRight: '10px' }} src="images/details_profile.png" alt="" />
-        홍길동
-      </DescName>
-      <Margin></Margin>
-      <TagContainer>
-        <Tag>#JAVA</Tag>
-        <Tag>#Spring Boot</Tag>
-        <Tag>#Back-End</Tag>
-        <Tag>#웹앱</Tag>
-        <Tag>#Spring</Tag>
-      </TagContainer>
-    </DescContainer>
-  );
-}
+	useEffect(() => {
+		// if (clickedId === 0) {
+		// 	// 전체보기
+		axios
+			.post(`${BASE_URL}/api/lectures`)
+			.then((res) => console.log(res.data.results))
+			.catch((err) => console.log(err));
+		// } else {
+		// 	// 카테고리 보기
+		// 	axios
+		// 		.post(`${BASE_URL}/api/findLectures/category/parent`, {
+		// 			parentCategoryId: clickedId,
+		// 		})
+		// 		.then((res) => setLectureList(res.data))
+		// 		.catch((err) => console.log(err));
+		// }
+	}, []);
+	return (
+		<DescContainer>
+			<DescCategory>개발 프로그래밍 {'>'} 웹 개발</DescCategory>
+			<Margin></Margin>
+			<DescSubject>
+				실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발
+			</DescSubject>
+			<Margin></Margin>
+			<DescName>
+				<img
+					style={{ marginRight: '10px' }}
+					src="images/details_profile.png"
+					alt=""
+				/>
+				홍길동
+			</DescName>
+			<Margin></Margin>
+			<TagContainer>
+				<Tag>#JAVA</Tag>
+				<Tag>#Spring Boot</Tag>
+				<Tag>#Back-End</Tag>
+				<Tag>#웹앱</Tag>
+				<Tag>#Spring</Tag>
+			</TagContainer>
+		</DescContainer>
+	);
+};
 
 const DescContainer = styled.div`
-  color: #fff;
-  margin-left: 40px;
+	color: #fff;
+	margin-left: 40px;
 `;
 const DescCategory = styled.div`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 21px;
+	font-family: Roboto;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 18px;
+	line-height: 21px;
 `;
 const DescSubject = styled.div`
-  font-family: NanumSquare;
-  font-style: normal;
-  font-weight: 800;
-  font-size: 32px;
-  line-height: 36px;
+	font-family: NanumSquare;
+	font-style: normal;
+	font-weight: 800;
+	font-size: 32px;
+	line-height: 36px;
 `;
 const DescName = styled.div`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 18px;
-  line-height: 21px;
+	font-family: Roboto;
+	font-style: normal;
+	font-weight: normal;
+	font-size: 18px;
+	line-height: 21px;
 `;
 const Margin = styled.div`
-  margin: 20px 0;
+	margin: 20px 0;
 `;
 const TagContainer = styled.div`
-  display: flex;
+	display: flex;
 `;
-
 
 export default BannerDescription;
