90656e4b50aebadcf0b6f998a82b91250bd75cd7
2je0 <82891332+2je0@users.noreply.github.com>
2022-01-27 18:21:33 +0900

    Update details.tsx

--- front-end/pages/details.tsx
+++ front-end/pages/details.tsx
@@ -1,14 +1,12 @@
 import Layout from '@components/Layout';
 import Banner from '@components/Details/Banner';
 import Menu from '@components/Details/Menu';
-import ContentSet from '@components/Details/ContentSet';
 import Footer from '@components/Details/Footer';
 
 const DetailsPage = () => (
 	<Layout>
 		<Banner />
 		<Menu />
-		{/* <ContentSet /> */}
 		<Footer />
 	</Layout>
 );
