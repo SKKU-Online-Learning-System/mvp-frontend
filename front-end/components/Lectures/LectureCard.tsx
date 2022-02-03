928795928249f2f8ceac410c846b26431a0b61f4
hkm <rbals0445@naver.com>
2022-02-04 00:30:39 +0900

    Update - Lecture 클릭시에 deatils로 이동 - 메인페이지에서 신규강의 없는경우 undefined 나오는 오류 해결 - LectureCard에서 클릭시 details로 아이디 넘겨줌

--- front-end/components/Lectures/LectureCard.tsx
+++ front-end/components/Lectures/LectureCard.tsx
@@ -1,13 +1,24 @@
+import { useRouter } from 'next/router';
+
 interface CardProps {
 	lectureInfo: string[];
 }
 
 const LectureCard = ({ lectureInfo }: CardProps) => {
+	const router = useRouter();
+	const handleClick = (id: number) => {
+		//router.push(`/details/${id}`);
+		router.push(`/details`);
+	};
+
 	return (
 		<>
 			{lectureInfo.map((i) => {
 				return (
 					<div
+						onClick={(event: React.MouseEvent<HTMLElement>) =>
+							handleClick(i.id)
+						}
 						key={i.id}
 						style={{ flex: '0 1 25%', padding: '1rem', cursor: 'pointer' }}
 					>
