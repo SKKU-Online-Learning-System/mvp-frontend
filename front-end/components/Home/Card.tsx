6e6ed81707da12b1fa59781bce9fea14e1809a9e
HKM <rbals0445@naver.com>
2022-03-20 18:32:32 +0900

    feat: 메인페이지에서 최신강의, 강의 추천기능 추가

--- front-end/components/Home/Card.tsx
+++ front-end/components/Home/Card.tsx
@@ -1,13 +1,13 @@
 interface CardProps {
 	title: string;
-	author: string;
+	description: string;
 }
 
-const Card = ({ title, author }: CardProps) => (
+const Card = ({ title, description }: CardProps) => (
 	<div style={{ width: '22%' }}>
 		<img style={{ width: '100%' }} src="images/card_img.png" />
 		<b>{title}</b>
-		<p>{author}</p>
+		<p>{description}</p>
 	</div>
 );
 
