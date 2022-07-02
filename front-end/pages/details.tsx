114d2830c8e79fd6269bfb45f49967c233e201bc
jylee <bulljy@naver.com>
2022-07-03 02:04:33 +0900

    feat(details/listcontent/lectureintroduction.tsx): 강의 소개 구현 (더미 데이터)

--- front-end/pages/details.tsx
+++ front-end/pages/details.tsx
@@ -1,12 +1,14 @@
 import LearningStatus from '@components/Details/ListContents/LearningStatus/LearningStatus';
+import LectureIntroduction from '@components/Details/ListContents/LectureIntroduction/LectureIntroduction';
 import MainBanner from '@components/Details/MainBanner/MainBanner';
 import Layout from '@components/Layout';
 
 const DetailsPage = () => (
 	<Layout>
 		<MainBanner />
+		<div>{/* <LearningStatus /> */}</div>
 		<div>
-			<LearningStatus />
+			<LectureIntroduction />
 		</div>
 	</Layout>
 );
