3aa95fd24f8a1bcc5956cc1ecb2a2965837f8170
jylee <bulljy@naver.com>
2022-07-03 14:24:50 +0900

    feat(details/listcontent/curriculum.tsx): 커리큘럼 컴포넌트 구현(더미데이터)

--- front-end/pages/details.tsx
+++ front-end/pages/details.tsx
@@ -1,3 +1,4 @@
+import Curriculum from '@components/Details/ListContents/Curriculum/Curriculum';
 import LearningStatus from '@components/Details/ListContents/LearningStatus/LearningStatus';
 import LectureIntroduction from '@components/Details/ListContents/LectureIntroduction/LectureIntroduction';
 import MainBanner from '@components/Details/MainBanner/MainBanner';
@@ -7,9 +8,10 @@ const DetailsPage = () => (
 	<Layout>
 		<MainBanner />
 		<div>{/* <LearningStatus /> */}</div>
-		<div>
-			<LectureIntroduction />
-		</div>
+		<div>{/* <LectureIntroduction /> */}</div>
+		<span>
+			<Curriculum />
+		</span>
 	</Layout>
 );
 
