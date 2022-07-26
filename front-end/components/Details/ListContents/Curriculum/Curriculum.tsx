abccdd4a1f6235f2c0a9913ab303e3a98a907d7a
2je0 <bulljy@naver.com>
2022-07-26 15:08:32 +0900

    style: 커리큘럼 table 사이즈 수정, font-size 수정

--- front-end/components/Details/ListContents/Curriculum/Curriculum.tsx
+++ front-end/components/Details/ListContents/Curriculum/Curriculum.tsx
@@ -1,119 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-const DUMMY_DATA = [
-	{ header: '스프링부트 이해와 실전' },
-	{
-		id: '1강',
-		title: '스프링부트란? ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '2강',
-		title: '스프링부트 개발 환경 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '3강',
-		title: '스프링부트 개발 입문1 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '4강',
-		title: '스프링부트 개발 입문2 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '5강',
-		title: '스프링부트 실전 활용 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '6강',
-		title: '스프링부트 실전 심화',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{ header: '스프링부트 이해와 실전' },
-	{
-		id: '1강',
-		title: '스프링부트란? ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '2강',
-		title: '스프링부트 개발 환경 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '3강',
-		title: '스프링부트 개발 입문1 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '4강',
-		title: '스프링부트 개발 입문2 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '5강',
-		title: '스프링부트 실전 활용 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '6강',
-		title: '스프링부트 실전 심화',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{ header: '스프링부트 이해와 실전' },
-	{
-		id: '1강',
-		title: '스프링부트란? ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '2강',
-		title: '스프링부트 개발 환경 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '3강',
-		title: '스프링부트 개발 입문1 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '4강',
-		title: '스프링부트 개발 입문2 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '5강',
-		title: '스프링부트 실전 활용 ',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-	{
-		id: '6강',
-		title: '스프링부트 실전 심화',
-		teacher: '강사 홍길동',
-		time: '17:23',
-	},
-];
 const Curriculum = () => {
 	const courceId = 1;
 	const [lectureData, setLectureData] = useState([
@@ -149,39 +36,38 @@ const Curriculum = () => {
 				</div>
 				<h2>강의 커리큘럼</h2>
 			</header>
-			<table>
-				{lectureData.map((ele) => {
-					return (
-						<>
-							<thead>
-								<tr>
-									<th colSpan={5}>{ele.title}</th>
-								</tr>
-							</thead>
-							<tbody>
-								{ele.lectures.map((lecture) => {
-									const hour =
-										lecture.duration / 3600 < 1
-											? ''
-											: `${lecture.duration / 3600}:`;
-									const duration = `${hour}${
-										lecture.duration / 60
-									}:${leadingZeros(lecture.duration % 60, 2)}`;
-									return (
-										<tr>
-											<td>{`${lecture.id}강`}</td>
-											<td>{lecture.title}</td>
-											<td>{lecture.teacher || '강사이름'}</td>
-											<td>{duration}</td>
-											<td>{`버튼`}</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</>
-					);
-				})}
-			</table>
+
+			{lectureData.map((ele) => {
+				return (
+					<table key={ele.title + 'table'}>
+						<thead>
+							<tr key={ele.title + 'head'}>
+								<th colSpan={5}>{ele.title}</th>
+							</tr>
+						</thead>
+						<tbody>
+							{ele.lectures.map((lecture) => {
+								const hour =
+									lecture.duration / 3600 < 1
+										? ''
+										: `${lecture.duration / 3600}:`;
+								const duration = `${hour}${
+									lecture.duration / 60
+								}:${leadingZeros(lecture.duration % 60, 2)}`;
+								return (
+									<tr key={lecture.id + lecture.title}>
+										<td>{`${lecture.id}강`}</td>
+										<td>{lecture.title}</td>
+										<td>{lecture.teacher || '강사이름'}</td>
+										<td>{duration}</td>
+										<td>{`버튼`}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				);
+			})}
 		</Container>
 	);
 };
@@ -189,23 +75,12 @@ const Curriculum = () => {
 export default Curriculum;
 
 const Container = styled.div`
-	width: 1197px;
+	width: 80%;
+	margin: auto;
 	padding: 18px 23px;
 	font-family: 'Noto Sans KR';
 	& header {
 		margin: 0 0 16px 18px;
-	}
-	& li {
-		border-bottom: solid;
-		border-color: #dfdfdf;
-		color: #393939;
-		font-size: 0.95rem;
-		margin: 3px 0;
-	}
-	& p {
-		color: #bcbcbc;
-		font-size: 0.8rem;
-		font-weight: bold;
 	}
 	& ul {
 		margin: 0;
@@ -222,6 +97,7 @@ const Container = styled.div`
 	}
 	& table {
 		width: 100%;
+		font-size: 0.8rem;
 	}
 
 	& table th {
