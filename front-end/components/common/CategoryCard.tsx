88b8ff3c614f487dea7f50338796f47b0855843b
2je0 <bulljy@naver.com>
2022-07-27 18:14:30 +0900

    feat: 공통 컴포넌트 폴더 common 생성, CategoryCard 구현

---
+++ front-end/components/common/CategoryCard.tsx
@@ -0,0 +1,63 @@
+import React, { Children } from 'react';
+import styled from 'styled-components';
+interface CardProps {
+	children?: React.ReactNode;
+	header: string;
+	smallHeader: string;
+	img: string;
+}
+const CategoryCard = ({ header, children, smallHeader }: CardProps) => {
+	return (
+		<CardWrapper>
+			<div className="header">
+				<div className="small-header"> {smallHeader}</div>
+				<img
+					style={{ right: 0, top: 0, position: 'absolute', cursor: 'pointer' }}
+					src={'images/plus_icon.png'}
+					width="24px"
+					onClick={() => alert('plus')}
+				></img>
+				{header || ''}
+			</div>
+			<CardContentWrapper>
+				<CardChildren>{children}</CardChildren>
+			</CardContentWrapper>
+		</CardWrapper>
+	);
+};
+
+export default CategoryCard;
+const CardWrapper = styled.div`
+	.header {
+		position: relative;
+		background: rgba(0, 0, 0, 0.7);
+		color: white;
+		min-width: 180px;
+		padding: 10px 20px 10px 20px;
+		font-size: 1.2rem;
+		font-weight: bold;
+		white-space: nowrap;
+	}
+	.small-header {
+		font-size: 0.7rem;
+		color: #8d8e8e;
+	}
+	display: flex;
+	flex-direction: column;
+	max-width: 300px;
+`;
+const CardContentWrapper = styled.div`
+	background-color: white;
+	padding-bottom: 20px;
+	border: solid;
+	border-color: #e2e2e2;
+	border-width: thin;
+`;
+
+const CardChildren = styled.div`
+	margin: 0 20px;
+	padding: 8px 0;
+	&:not(:last-child) {
+		border-bottom: 1px solid gray;
+	}
+`;
--- front-end/pages/my-page/index.tsx
+++ front-end/pages/my-page/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Layout from '@components/MyPage/Layout';
+import CategoryCard from '@components/common/CategoryCard';
 const index = () => {
-	return <Layout>index</Layout>;
+	return (
+		<Layout>
+			<CategoryCard smallHeader="DASHBOARD" header="MY PROFILE">
+				afsd
+			</CategoryCard>
+		</Layout>
+	);
 };
 
 export default index;
