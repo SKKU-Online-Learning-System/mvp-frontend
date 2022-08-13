742182347dd1ae6c29b96ac4e1b3313438b316ca
2je0 <bulljy@naver.com>
2022-08-13 17:02:59 +0900

    fix: signup modal 창 fixed 되지 않는 문제 position: fixed로 해결, backdrop 클릭하면 모달창 없애주는 기능 추가

--- front-end/components/modals/SignUpModal.tsx
+++ front-end/components/modals/SignUpModal.tsx
@@ -1,81 +1,85 @@
-import React, { useEffect, useRef, useState } from "react";
-import ReactDOM from "react-dom";
-import styled from "styled-components";
-import SignUpForm from "./SignUpForm";
+import React, { useEffect, useRef, useState } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import SignUpForm from './SignUpForm';
 
 interface SignUpModalProps {
-  show: boolean;
-  onClose: Function;
-  children?: React.ReactNode;
+	show: boolean;
+	onClose: Function;
+	children?: React.ReactNode;
 }
 
 function SignUpModal({ show, onClose, children }: SignUpModalProps) {
-  const [isBrowser, setIsBrowser] = useState(false);
+	const [isBrowser, setIsBrowser] = useState(false);
+	useEffect(() => {
+		setIsBrowser(true);
+	}, []);
 
-  useEffect(() => {
-    setIsBrowser(true);
-  }, []);
+	const handleCloseClick = (e: any) => {
+		e.preventDefault();
+		onClose();
+	};
 
-  const handleCloseClick = (e: any) => {
-    e.preventDefault();
-    onClose();
-  };
+	const modalContent = show ? (
+		<StyledModalOverlay>
+			<StyledModal>
+				<StyledModalHeader>
+					<a href="#" onClick={handleCloseClick}>
+						x
+					</a>
+				</StyledModalHeader>
 
-  const modalContent = show ? (
-    <StyledModalOverlay>
-      <StyledModal>
-        <StyledModalHeader>
-          <a href="#" onClick={handleCloseClick}>
-            x
-          </a>
-        </StyledModalHeader>
+				<StyledModalBody>
+					<h1>Sign Up</h1>
+					<SignUpForm />
+				</StyledModalBody>
+			</StyledModal>
+			<Backdrop onClick={handleCloseClick} />
+		</StyledModalOverlay>
+	) : null;
 
-        <StyledModalBody>
-          <h1>Sign Up</h1>
-          <SignUpForm />
-        </StyledModalBody>
-      </StyledModal>
-    </StyledModalOverlay>
-  ) : null;
-
-  if (isBrowser) {
-    const portalDiv = document.getElementById("modal-root")!;
-    return ReactDOM.createPortal(
-      modalContent,
-      portalDiv
-    );
-  } else {
-    return null;
-  }
+	if (isBrowser) {
+		const portalDiv = document.getElementById('modal-root')!;
+		return ReactDOM.createPortal(modalContent, portalDiv);
+	} else {
+		return null;
+	}
 }
 
 export default SignUpModal;
-
+const Backdrop = styled.div`
+	background-color: rgba(0, 0, 0, 0.5);
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+`;
 const StyledModalBody = styled.div`
-  padding-top: 10px;
+	padding-top: 10px;
 `;
 
 const StyledModalHeader = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  font-size: 25px;
+	display: flex;
+	justify-content: flex-end;
+	font-size: 25px;
 `;
 
 const StyledModal = styled.div`
-  background: white;
-  width: 500px;
-  height: 600px;
-  border-radius: 15px;
-  padding: 15px;
+	z-index: 2;
+	background: white;
+	width: 500px;
+	height: 600px;
+	border-radius: 15px;
+	padding: 15px;
 `;
 const StyledModalOverlay = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
