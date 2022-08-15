e612cfed41192bf6af4a8b95796110befb2404b9
GyuMin Han <60285506+rbals0445@users.noreply.github.com>
2022-08-15 18:29:35 +0900

    Update lectureApi.ts

--- front-end/apis/Lectures/lectureApi.ts
+++ front-end/apis/Lectures/lectureApi.ts
@@ -1,21 +1,21 @@
 import axiosInstance from '..';
 
 export const fetchAllLectureCategories = () => {
-	return axiosInstance.get(`/courses/categories`);
+	return axiosInstance.get(`courses/categories`);
 };
 
 export const fetchLectureLists = (category: string) => {
-	return axiosInstance.get(`/courses/search?category2Id=${category}`);
+	return axiosInstance.get(`courses/search?category2Id=${category}`);
 };
 
 export const fetchSearchedData = (name: string, difficulty: string) => {
 	if (difficulty) {
-		return axiosInstance.get(`/courses?s=${name}&difficulty=${difficulty}`);
+		return axiosInstance.get(`courses?s=${name}&difficulty=${difficulty}`);
 	} else {
-		return axiosInstance.get(`/courses/search?keyword=${name}`);
+		return axiosInstance.get(`courses/search?keyword=${name}`);
 	}
 };
 
 export const fetchAllLecturesPerPage = (pageNum: number) => {
-	return axiosInstance.get(`/courses/search?page=${pageNum}`);
+	return axiosInstance.get(`courses/search?page=${pageNum}`);
 };
