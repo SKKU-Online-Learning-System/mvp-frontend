4855f4c135d6ce4d86af38dcee8c7de48f49cf10
USER <user@AL02024981.local>
2022-09-14 03:20:17 +0900

    feat: add lecture history
    강의 기록 저장 추가. (현재 테스트 안됨)

--- front-end/pages/lectures/[lectureId].tsx
+++ front-end/pages/lectures/[lectureId].tsx
@@ -2,7 +2,12 @@ import ReactPlayer from 'react-player/lazy';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
-import { fetchLectureVideoUrl } from 'apis/Lectures/lectureApi';
+import {
+	fetchLectureVideoUrl,
+	fetchLectureHistory,
+	updateLectureHistory,
+} from 'apis/Lectures/lectureApi';
+import { useRef } from 'react';
 import { useRouter } from 'next/router';
 import { LecturePicker } from '@components/Lectures/LecturePicker';
 import Error from 'next/error';
@@ -11,17 +16,46 @@ import Error from 'next/error';
 const LecturePlayer = () => {
 	const router = useRouter();
 	const { lectureId, courseId } = router.query;
+	const ref = useRef(null);
 
 	const [videoUrl, setVideoUrl] = useState<string>('');
 
 	const _fetchLectureVideoUrl = async (lectureId: string) => {
-		const res = await fetchLectureVideoUrl(lectureId);
-		setVideoUrl(res.data.video_url);
+		try {
+			const res = await fetchLectureVideoUrl(lectureId);
+			setVideoUrl(res.data.video_url);
+		} catch (e: unknown) {
+			console.warn(e);
+		}
+	};
+
+	const _fetchLectureHistory = async (lectureId: string) => {
+		try {
+			const res = await fetchLectureHistory(lectureId);
+			console.log(res.data);
+		} catch (e: unknown) {
+			console.warn(e);
+		}
+	};
+
+	const _updateLectureHistory = async (currentPlayTime: number) => {
+		updateLectureHistory({
+			lectureId: lectureId as string,
+			lastTime: currentPlayTime,
+		});
+	};
+
+	const sendCurrentPlayTime = () => {
+		if (!ref) return;
+
+		const time = ~~ref.current.getCurrentTime();
+		_updateLectureHistory(time);
 	};
 
 	useEffect(() => {
 		if (!router.isReady) return;
 		_fetchLectureVideoUrl(lectureId as string);
+		// _fetchLectureHistory(lectureId as string);
 	}, [router.isReady, lectureId]);
 
 	return (
@@ -30,6 +64,7 @@ const LecturePlayer = () => {
 				<LecturePlayerWrapper>
 					<div className="player-wrapper">
 						<ReactPlayer
+							ref={ref}
 							className="react-player"
 							url={`https://mrdang.kro.kr/api/${videoUrl}`} // 플레이어 url
 							style={{ minWidth: '1080px', minHeight: '768px' }}
@@ -41,6 +76,10 @@ const LecturePlayer = () => {
 							poster={
 								'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
 							} // 플레이어 초기 포스터 사진
+							progressInterval={5000}
+							// onPause={() => sendCurrentPlayTime}
+							// onProgress={() => sendCurrentPlayTime}
+							// onEnded={() => sendCurrentPlayTime}
 						/>
 					</div>
 					{router.isReady && <LecturePicker courseId={courseId as string} />}
