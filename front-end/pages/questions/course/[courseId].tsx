a6f4d0f848ad27976d7952ccbf40c85512e5aaef
gyumin_han <rbals0445@naver.com>
2022-10-15 03:14:28 +0900

    chore: build test

--- front-end/pages/questions/course/[courseId].tsx
+++ front-end/pages/questions/course/[courseId].tsx
@@ -4,33 +4,29 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import API from 'apis/Courses/courseApi';
-import {
-	setCourse,
-	setLectures,
-	setQna,
-} from 'store/feature/course/courseDetailSlice';
+import { setLectures, setQna } from 'store/feature/course/courseDetailSlice';
 import { useAppDispatch } from 'store/app/hooks';
 import QuestionForm from '@components/Questions/QuestionForm';
 import { ICourseDetail } from 'types/Course';
 const QuestionsByCoursePage = () => {
 	const router = useRouter();
 	const dispatch = useAppDispatch();
-	// const [openForm, setOpenForm] = useState(false);
+	const [openForm, setOpenForm] = useState(false);
 	const { courseId } = router.query;
 	const [courseDetail, setCourseDetail] = useState<ICourseDetail>();
 	useEffect(() => {
 		if (!router.isReady) return;
-		// (async () => {
-		// 	const course: any = await API.fetchCourseDetail(courseId as string);
-		// 	const lectures: any = await API.fetchCourseDetailLectures(
-		// 		courseId as string,
-		// 	);
-		// 	const qna: any = await API.fetchCourseDetailQna(courseId as string);
-		// 	setCourseDetail(course.data);
-		// 	dispatch(setLectures(lectures?.data));
-		// 	if (qna.data.length > 3) dispatch(setQna(qna.data.slice(0, 3)));
-		// 	else dispatch(setQna(qna?.data));
-		// })();
+		(async () => {
+			const course: any = await API.fetchCourseDetail(courseId as string);
+			const lectures: any = await API.fetchCourseDetailLectures(
+				courseId as string,
+			);
+			const qna: any = await API.fetchCourseDetailQna(courseId as string);
+			setCourseDetail(course.data);
+			dispatch(setLectures(lectures?.data));
+			if (qna.data.length > 3) dispatch(setQna(qna.data.slice(0, 3)));
+			else dispatch(setQna(qna?.data));
+		})();
 	}, [router.isReady, courseId]);
 	return (
 		<>
