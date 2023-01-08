858ba514984ac874b3e6fa4b148395aed9067196
gyumin_han <rbals0445@naver.com>
2023-01-08 17:14:23 +0900

    feat: 메인페이지 react-query 적용 완료

--- front-end/query/hooks/Main/index.ts
+++ front-end/query/hooks/Main/index.ts
@@ -1,11 +1,20 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { QUERY_KEYS } from 'query/keys/Main';
-import API from 'apis/Main';
+import mainPageAPI from 'apis/Main';
+import myPageAPI from 'apis/MyPage';
 import { ICourse } from 'types/Main';
+import { ILatestLecture } from 'types/MyPage';
 
 export const usePopularCoursesFetch = (): UseQueryResult<ICourse[]> => {
 	return useQuery<ICourse[]>(
 		[QUERY_KEYS.FETCH_POPULAR_COURSES],
-		API.fetchPopularCourses,
+		mainPageAPI.fetchPopularCourses,
+	);
+};
+
+export const useRecentLecturesFetch = (): UseQueryResult<ILatestLecture[]> => {
+	return useQuery<ILatestLecture[]>(
+		[QUERY_KEYS.FETCH_RECENT_LECTURES],
+		myPageAPI.fetchRecentLectures,
 	);
 };
