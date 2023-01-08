aff1a0004f5fe51eec2ae6807628d816656633f5
gyumin_han <rbals0445@naver.com>
2023-01-09 00:42:08 +0900

    chore: 사용하지 않는 파일 제거

--- front-end/query/keys/Main/index.ts
+++
@@ -1,4 +0,0 @@
-export const QUERY_KEYS = {
-	FETCH_POPULAR_COURSES: 'fetchPopularCourses',
-	FETCH_RECENT_LECTURES: 'fetchRecentLectures',
-};
--- front-end/store/feature/course/courseSelector.ts
+++
@@ -1,8 +0,0 @@
-// NOTE. https://github.com/reduxjs/reselect#api
-import { createSelector } from 'reselect';
-import { RootState } from 'store/app/store';
-
-export const selectCourseCategory = createSelector(
-	(state: RootState) => state.course.courseCategory,
-	(courseCategory) => courseCategory,
-);
