b8100051897d916828a8ebe47674459cc752f0ca
RSuhyeon <fbtngus2789@naver.com>
2023-05-09 23:18:31 +0900

    Header 컴포넌트 TailwindCSS 이전 작업 완료

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,11 +1,13 @@
+import { DEVICE_BREAKPOINT } from './constants/breakpoint';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./components/*.{js, ts, jsx, tsx}',
-		'./components/*/*.{js, ts, jsx, tsx}',
-	],
+	content: [],
 	purge: ['./components/**/*.tsx', './components/*.tsx'],
 	theme: {
+		screens: {
+			dt: { max: DEVICE_BREAKPOINT.DESKTOP },
+		},
 		extend: {},
 	},
 	plugins: [],
