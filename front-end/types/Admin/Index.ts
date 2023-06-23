507b51419afbb63e21e04a512a4a3826683c908d
RSuhyeon <fbtngus2789@naver.com>
2023-06-23 16:45:03 +0900

    Query response 처리를 위한 신규 컨텐츠의 데이터 type 생성

---
+++ front-end/types/Admin/Index.ts
@@ -0,0 +1,12 @@
+export interface INewCourseInfo {
+	category1Id: number;
+	category2Id: number;
+	createdAt: string;
+	description: string;
+	difficulty: number;
+	id: number;
+	instructor: string;
+	summary: string;
+	thumbnail: string;
+	title: string;
+}
