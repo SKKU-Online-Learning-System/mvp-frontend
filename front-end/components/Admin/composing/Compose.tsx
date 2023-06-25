62dd50d1f6632ffaf7a2be58f3e0047983724a46
RSuhyeon <fbtngus2789@naver.com>
2023-06-25 19:23:14 +0900

    기타 작업

--- front-end/components/Admin/composing/Compose.tsx
+++ front-end/components/Admin/composing/Compose.tsx
@@ -6,7 +6,7 @@ const Compose = () => {
 	return (
 		<div
 			id="contents"
-			className="grid grid-cols-2 grid-rows-2 mt-14 gap-x-20 gap-y-20 pb-[100px] min-h-full"
+			className="grid min-h-full grid-cols-2 grid-rows-2 pb-24 mt-14 gap-x-20 gap-y-20"
 		>
 			<AdminCard title="인기 컨텐츠" />
 			<AdminCard title="신규 컨텐츠" />
