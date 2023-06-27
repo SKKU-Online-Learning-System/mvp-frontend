8f79479111a114b1073aa943b1fc9c042440645e
RSuhyeon <fbtngus2789@naver.com>
2023-06-27 17:32:42 +0900

    강좌 등록 컴포넌트 작업 시작

---
+++ front-end/components/Admin/contents-manage/CourseRegisterCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CourseRegisterCard = () => {
+	return (
+		<div className="w-3/6">
+			<div id="upper" className="flex w-3/6 bg-red">
+				<span>Operating</span>
+				<h3>title</h3>
+				<span>Instructor</span>
+			</div>
+			<div id="lower"></div>
+		</div>
+	);
+};
+
+export default CourseRegisterCard;
