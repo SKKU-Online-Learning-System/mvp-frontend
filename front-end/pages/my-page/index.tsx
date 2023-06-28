e32a4ccb4a7c617f784c2d408e5bf2783f24b56c
RSuhyeon <fbtngus2789@naver.com>
2023-06-28 18:37:12 +0900

    reset

--- front-end/pages/my-page/index.tsx
+++ front-end/pages/my-page/index.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
 
-import CategoryCard from '@components/common/CategoryCard';
 import MyPageLayout from '@components/MyPage/MyPageLayout';
 import withRouteGuard from '@components/withRouteGuard';
 
@@ -17,66 +16,42 @@ const MyPageIndex = (): ReactElement => {
 	return (
 		<MyPageLayout>
 			<Container>
-				<CategoryCard
-					smallHeader="DASHBOARD"
-					header="MY PROFILE"
-					margin={margin}
-				>
+				<div smallHeader="DASHBOARD" header="MY PROFILE" margin={margin}>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae,
 					expedita et. Soluta pariatur impedit, earum amet quis eum asperiores
 					sed iste, assumenda voluptates ducimus quaerat aperiam nihil labore,
 					repellat quam.
-				</CategoryCard>
-				<CategoryCard
-					smallHeader="DASHBOARD"
-					header="MY PROFILE"
-					margin={margin}
-				>
+				</div>
+				<div smallHeader="DASHBOARD" header="MY PROFILE" margin={margin}>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae,
 					expedita et. Soluta pariatur impedit, earum amet quis eum asperiores
 					sed iste, assumenda voluptates ducimus quaerat aperiam nihil labore,
 					repellat quam.
-				</CategoryCard>
-				<CategoryCard
-					smallHeader="DASHBOARD"
-					header="MY PROFILE"
-					margin={margin}
-				>
+				</div>
+				<div smallHeader="DASHBOARD" header="MY PROFILE" margin={margin}>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae,
 					expedita et. Soluta pariatur impedit, earum amet quis eum asperiores
 					sed iste, assumenda voluptates ducimus quaerat aperiam nihil labore,
 					repellat quam.
-				</CategoryCard>
-				<CategoryCard
-					smallHeader="DASHBOARD"
-					header="MY PROFILE"
-					margin={margin}
-				>
+				</div>
+				<div smallHeader="DASHBOARD" header="MY PROFILE" margin={margin}>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae,
 					expedita et. Soluta pariatur impedit, earum amet quis eum asperiores
 					sed iste, assumenda voluptates ducimus quaerat aperiam nihil labore,
 					repellat quam.
-				</CategoryCard>
-				<CategoryCard
-					smallHeader="DASHBOARD"
-					header="MY PROFILE"
-					margin={margin}
-				>
+				</div>
+				<div smallHeader="DASHBOARD" header="MY PROFILE" margin={margin}>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae,
 					expedita et. Soluta pariatur impedit, earum amet quis eum asperiores
 					sed iste, assumenda voluptates ducimus quaerat aperiam nihil labore,
 					repellat quam.
-				</CategoryCard>
-				<CategoryCard
-					smallHeader="DASHBOARD"
-					header="MY PROFILE"
-					margin={margin}
-				>
+				</div>
+				<div smallHeader="DASHBOARD" header="MY PROFILE" margin={margin}>
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae,
 					expedita et. Soluta pariatur impedit, earum amet quis eum asperiores
 					sed iste, assumenda voluptates ducimus quaerat aperiam nihil labore,
 					repellat quam.
-				</CategoryCard>
+				</div>
 			</Container>
 		</MyPageLayout>
 	);
