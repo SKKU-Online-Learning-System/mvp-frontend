788f9870649d1e5a3e04db2b8ebf31b9ce5de702
RSuhyeon <fbtngus2789@naver.com>
2023-06-28 16:32:36 +0900

    AdminCard 컴포넌트 react-query 도입 및 코드 구조 단순화

--- front-end/components/Admin/composing/AdminCard.tsx
+++ front-end/components/Admin/composing/AdminCard.tsx
@@ -1,32 +1,22 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRotateForward } from '@fortawesome/free-solid-svg-icons';
 
-import API from '../../../apis/Admin/adminAPI';
-import { INewCourseInfo, ICourseRetrieveInfo } from 'types/Admin/Index';
-import { Category1Seed } from '../../../constants/category1.seed';
-// import { usePopularCoursesFetch } from 'query/hooks/Admin/index';
-// Todo: react-query 사용법 알아보고 적용시키기
-// Todo: Main Page Popular Contents도 해당 query hook으로 바꿀 것
+import {
+	usePopularCoursesFetch,
+	useNewCoursesFetch,
+} from 'query/hooks/Admin/index';
 
 const selectBtnStyle =
 	'ml-4 p-2 text-lg font-semibold bg-[#e2e3e4] rounded-lg hover:bg-[#929394] duration-150';
 const tableContentsStyle = 'flex items-center mt-6 text-center justify-normal';
-const optionStyle = 'font-extrabold text-xl focus:border-0';
 
 const AdminCard = ({ title }: { title: string }): ReactElement => {
-	const [contents, setContents] = useState<ICourseRetrieveInfo[]>([]);
-	const [newContents, setNewContents] = useState<INewCourseInfo[]>([]);
+	const { data: popularContents, isLoading: isPopularCoursesLoading } =
+		usePopularCoursesFetch(title === '인기 컨텐츠' ? '' : title);
+	const { data: newContents, isLoading: isNewCoursesLoading } =
+		useNewCoursesFetch();
 	const [disabled, setDisabled] = useState<boolean>(false);
-
-	const handleContentsRetrieve = async (title: string) => {
-		const data = await API.fetchPopularContentsInfo(title);
-		return data;
-	};
-	const handleNewContentsRetrieve = async () => {
-		const data = await API.fetchNewContentsInfo();
-		return data;
-	};
 
 	const onRefreshBtnClick = () => {
 		if (!disabled) {
@@ -37,24 +27,13 @@ const AdminCard = ({ title }: { title: string }): ReactElement => {
 		}
 	};
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const popularContents =
-				title === '인기 컨텐츠'
-					? await handleContentsRetrieve('')
-					: await handleContentsRetrieve(title);
-			const recentContents = await handleNewContentsRetrieve();
-
-			setContents(popularContents);
-			setNewContents(recentContents);
-		};
-
-		fetchData();
-	}, []);
+	if (isPopularCoursesLoading || isNewCoursesLoading) {
+		return <h2>Loading . . .</h2>;
+	}
 
 	// Todo: 선택 버튼 function 제작 -> Backend 측에 Main Banner 관련 API 제작 요청
 	return (
-		<div className="flex flex-col h-full p-12 shadow-xl rounded-xl bg-[#fff] min-w-[768px]">
+		<div className="flex flex-col h-full p-12 shadow-xl rounded-xl bg-white min-w-[768px]">
 			<h3 className="pl-10 mb-10 text-3xl font-extrabold select-none">
 				{title === '인기 컨텐츠' ? (
 					<div>
@@ -67,18 +46,8 @@ const AdminCard = ({ title }: { title: string }): ReactElement => {
 						</button>
 						<span>{title}</span>
 					</div>
-				) : title === '신규 컨텐츠' ? (
-					title
 				) : (
-					<select className="px-2 py-1 transition duration-200 rounded-xl hover:shadow-[1px_2px_4px_-1px]">
-						{Category1Seed.map((item, idx) => {
-							return (
-								<option key={idx} className={optionStyle} value={item.id}>
-									{item.name}
-								</option>
-							);
-						})}
-					</select>
+					title
 				)}
 			</h3>
 			<table className="w-full h-fit">
@@ -94,14 +63,14 @@ const AdminCard = ({ title }: { title: string }): ReactElement => {
 				</thead>
 				<tbody className="flex flex-col">
 					{title === '신규 컨텐츠'
-						? newContents.map((item, idx) => {
+						? newContents?.map((item, idx) => {
 								return (
 									<tr key={idx} className={tableContentsStyle}>
 										<td className="w-1/5">{item.createdAt.split('T')[0]}</td>
 										<td className="w-1/3">{item.title}</td>
 										<td className="w-1/6 ">{item.instructor}</td>
 										<button
-											onClick={handleNewContentsRetrieve}
+											// onClick={handleNewContentsRetrieve}
 											className={selectBtnStyle}
 										>
 											선택
@@ -109,7 +78,7 @@ const AdminCard = ({ title }: { title: string }): ReactElement => {
 									</tr>
 								);
 						  })
-						: contents.map((item, idx) => {
+						: popularContents?.map((item, idx) => {
 								return (
 									<tr key={idx} className={tableContentsStyle}>
 										<td className="w-1/12">{idx + 1}</td>
@@ -120,7 +89,7 @@ const AdminCard = ({ title }: { title: string }): ReactElement => {
 										<td className="w-1/6 ">{item.instructorName}</td>
 										<td className="w-1/12">{item.enrollmentCount}</td>
 										<button
-											onClick={handleNewContentsRetrieve}
+											// onClick={handleNewContentsRetrieve}
 											className={selectBtnStyle}
 										>
 											선택
--- front-end/query/hooks/Admin/index.ts
+++ front-end/query/hooks/Admin/index.ts
@@ -1,18 +1,27 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
+import { ICourseRetrieveInfo, INewCourseInfo } from 'types/Admin/Index';
 import { QUERY_KEYS } from 'query/hooks/Admin/queryKeys';
 import adminAPI from 'apis/Admin/adminAPI';
-import { ICourseRetrieveInfo } from 'types/Admin/Index';
 
-export const usePopularCoursesFetch = (): UseQueryResult<
-	ICourseRetrieveInfo[]
-> => {
+export const usePopularCoursesFetch = (
+	title: string,
+): UseQueryResult<ICourseRetrieveInfo[]> => {
 	return useQuery<ICourseRetrieveInfo[]>(
-		[QUERY_KEYS.FETCH_POPULAR_COURSES],
-		adminAPI.fetchPopularContentsInfo,
+		[QUERY_KEYS.FETCH_POPULAR_COURSES, title],
+		() => adminAPI.fetchPopularContentsInfo(title),
 		{
-			staleTime: 60 * 1000,
-			keepPreviousData: true,
+			staleTime: 60 * 60 * 1000,
+		},
+	);
+};
+
+export const useNewCoursesFetch = (): UseQueryResult<INewCourseInfo[]> => {
+	return useQuery<INewCourseInfo[]>(
+		[QUERY_KEYS.FETCH_NEW_LECTURES],
+		() => adminAPI.fetchNewContentsInfo(),
+		{
+			staleTime: 10 * 60 * 1000,
 		},
 	);
 };
