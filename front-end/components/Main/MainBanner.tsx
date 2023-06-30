f4855ab1b4e16fddded479eed270e95178d247a8
sonah5009 <sonah5009@gmail.com>
2023-06-30 18:39:20 +0900

    Edit: MainBanner 썸네일 연동 usePopularCoursesFetch

--- front-end/components/Main/MainBanner.tsx
+++ front-end/components/Main/MainBanner.tsx
@@ -12,30 +12,15 @@ import { RxDotFilled } from 'react-icons/rx';
 
 // TODO. query string 형식으로 요청 보내기
 
+import { ICourse } from 'types/Main';
+import { usePopularCoursesFetch } from 'query/hooks/Main/index';
+
+// interface ICourseCardProps {
+// 	course: ICourse;
+// }
+
 const Curation = () => {
 	// 동그라미들: 카테고리로 연결하는 역할
-	// const router = useRouter();
-	// const [bannerList, setBannerList] = useState<IMainBanners[]>([]);
-
-	// const getImageUrl = (path: string) => {
-	// 	const url = new URL(`${process.env.NEXT_PUBLIC_API_SERVER}`);
-	// 	if (url.pathname === '/') url.pathname = path;
-	// 	else {
-	// 		url.pathname = `${url.pathname}${path}`;
-	// 	}
-
-	// 	return url.toString();
-	// };
-
-	// const handleCircleClick = (category2sId: number | null) => () => {
-	// 	router.push({ pathname: '/courses', query: { category2sId } });
-	// };
-
-	// useEffect(() => {
-	// 	API.fetchBannerImgUrls()
-	// 		.then((res) => setBannerList(res.data))
-	// 		.catch((err) => console.log(err));
-	// }, []);
 	const navigationLinks = [
 		{ category: '프로그래밍 언어', url: './courses?category2sId=6' },
 		{ category: '인공지능', url: './courses?category2sId=18' },
@@ -46,6 +31,7 @@ const Curation = () => {
 
 	return (
 		<div className="my-4">
+			{/* <CourseList /> */}
 			<ul className="flex justify-center space-x-10 text-center place-items-center">
 				{navigationLinks.map((link, index) => (
 					<a key={index} href={link.url}>
@@ -56,46 +42,19 @@ const Curation = () => {
 					</a>
 				))}
 			</ul>
-			{/* <ul className="flex justify-center space-x-10 text-center place-items-center">
-				{bannerList.length > 0 &&
-					bannerList.map((banner, index) => (
-						<div
-							key={index}
-							className="pl-[10px] overflow-auto cursor-pointer"
-							onClick={handleCircleClick(banner.category2Id)}
-						>
-							<img
-								crossOrigin="anonymous"
-								src={getImageUrl(banner.filename)}
-								alt="bannerItem"
-								className="max-w-[250px] w-[250px] h-[250px]"
-							/>
-						</div>
-					))}
-			</ul> */}
 		</div>
 	);
 };
 
 const MainBanner = () => {
-	const slides = [
-		{
-			url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-		},
-		{
-			url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-		},
-		{
-			url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-		},
-
-		{
-			url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-		},
-		{
-			url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-		},
-	];
+	// 매인배너 썸네일 가져오기
+	const { data: popularCourses } = usePopularCoursesFetch();
+	const slides: { thumbnail: string; url: string }[] = (
+		popularCourses ?? []
+	).map((course: ICourse) => ({
+		thumbnail: course.thumbnail,
+		url: `/courses/${course.id}`,
+	}));
 
 	const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -116,12 +75,18 @@ const MainBanner = () => {
 	};
 
 	return (
-		// <div>
 		<div className="h-[680px] w-full group bg-[var(--color-green-700)]">
+			{/* <CourseList /> */}
 			<div
-				style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+				style={{
+					backgroundImage: `url(${slides[currentIndex]?.thumbnail})`,
+				}}
 				className="w-full h-full duration-500 bg-center bg-cover max-w-[1400px] m-auto py-16 px-4 relative"
 			>
+				<a
+					href={slides[currentIndex]?.url}
+					className="absolute inset-0 w-full h-full"
+				/>
 				<div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
 					<BsChevronCompactLeft onClick={prevSlide} size={30} />
 				</div>
@@ -129,6 +94,7 @@ const MainBanner = () => {
 					<BsChevronCompactRight onClick={nextSlide} size={30} />
 				</div>
 			</div>
+			{/* 위치 표시해주는 동그라미들 */}
 			<div className="flex justify-center py-2 top-4">
 				{slides.map((slide, slideIndex) => (
 					<div
@@ -148,7 +114,6 @@ const MainBanner = () => {
 			</div>
 			<Curation />
 		</div>
-		// </div>
 	);
 };
 
