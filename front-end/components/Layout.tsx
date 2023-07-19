b2f516f8f5f48976d827d6632e29f480d20d54a5
RSuhyeon <fbtngus2789@naver.com>
2023-07-19 15:05:01 +0900

    white(#fff)로 배경색 통일

--- front-end/components/Layout.tsx
+++ front-end/components/Layout.tsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 import ErrorManager from '../pages/ErrorManager';
 
 const Layout = ({ children }: { children?: ReactElement[] }) => (
-	<div className="flex flex-col bg-[var(--color-Background)] w-screen h-screen max-w-full">
+	<div className="flex flex-col w-screen h-screen max-w-full bg-white">
 		<Header />
 		<ErrorManager>
 			<div className="flex-1 pb-15">{children}</div>
