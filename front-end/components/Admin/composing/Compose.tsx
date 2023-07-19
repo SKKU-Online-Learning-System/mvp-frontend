9d7f4983d846ca708481a435a11e44831105a95e
RSuhyeon <fbtngus2789@naver.com>
2023-07-19 15:05:29 +0900

    메인페이지 섹션 title에 맞춰 Admin 편성 카드 title 변경

--- front-end/components/Admin/composing/Compose.tsx
+++ front-end/components/Admin/composing/Compose.tsx
@@ -8,8 +8,8 @@ const Compose = () => {
 		<div className="grid w-full min-h-full grid-cols-2 grid-rows-2 p-10 pb-24 mt-14 gap-x-20 gap-y-20">
 			<PopularContentsCard title="인기 컨텐츠" order={0} />
 			<NewContentsCard title="신규 컨텐츠" />
-			<PopularContentsCard title="데이터 사이언스" order={2} />
-			<PopularContentsCard title="교양" order={3} />
+			<PopularContentsCard title="인공지능" order={2} />
+			<PopularContentsCard title="프로그래밍" order={3} />
 		</div>
 	);
 };
