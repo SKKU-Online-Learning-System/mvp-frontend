017e24564886e3387a8872975c93c442bd865a66
sonah5009 <sonah5009@gmail.com>
2023-07-31 22:51:53 +0900

    마이페이지 수강완료강좌

--- front-end/components/MyPage/Completed.tsx
+++ front-end/components/MyPage/Completed.tsx
@@ -22,33 +22,42 @@ const Completed = () => {
 
 	return (
 		<MyPageLayout>
-			<BreadCrumb
-				category={'MY PAGE'}
-				menu={menu}
-				containerPadding={'1rem 0'}
-			/>
-			<MyPageTitle title={MYPAGE_MENU.COMPLETED_WATCHING_LECTURES} />
-			<div className="grid gap-x-4 gap-y-4 border-[1px] border-solid border-gray-700 grid-rows-3 grid-cols-4 p-5">
-				{!completedCourseList?.length ? (
-					<div>완료된 강의가 없습니다.</div>
-				) : (
-					completedCourseList?.map((elem, index) => (
-						<div
-							className="w-full overflow-hidden relative cursor-pointer"
-							onClick={handleClick(elem.course.id)}
-							key={index}
-						>
-							<img
-								className="aspect-video"
-								width={'100%'}
-								src={elem.course.thumbnail}
-							></img>
-							<div className="text-base text-ellipsis overflow-hidden whitespace-nowrap">
-								{elem.course.title}
-							</div>
-						</div>
-					))
-				)}
+			<div className="bg-[var(--color-mrgreen-9)] w-full py-4">
+				{/* <BreadCrumb
+					category={'MY PAGE'}
+					menu={menu}
+					containerPadding={'1rem 0'}
+				/> */}
+			</div>
+
+			{/* <MyPageTitle title={MYPAGE_MENU.COMPLETED_WATCHING_LECTURES} /> */}
+			<div className="bg-[var(--color-mrgreen-9)]  w-full ">
+				{/* <div className="grid gap-x-4 gap-y-4 border-[1px] border-solid border-gray-700 grid-rows-3 grid-cols-4 p-5"> */}
+				<div className="bg-[var(--color-Surface)] rounded-tl-lg">
+					<div className="grid grid-cols-4 p-5 mx-56 mb-32 tbl:mx-auto dt:grid-cols-3 tbl:grid-cols-3 mbl:grid-cols-1 gap-x-4 gap-y-4">
+						{!completedCourseList?.length ? (
+							<div>완료된 강의가 없습니다.</div>
+						) : (
+							completedCourseList?.map((elem, index) => (
+								<div
+									// className="relative w-full overflow-hidden cursor-pointer"
+									className="rounded-md w-full mt-5 overflow-hidden relative cursor-pointer transition hover:scale-[1.03] bg-white"
+									onClick={handleClick(elem.course.id)}
+									key={index}
+								>
+									<img
+										className="aspect-video"
+										width={'100%'}
+										src={elem.course.thumbnail}
+									/>
+									<div className="overflow-hidden text-base text-ellipsis whitespace-nowrap">
+										{elem.course.title}
+									</div>
+								</div>
+							))
+						)}
+					</div>
+				</div>
 			</div>
 		</MyPageLayout>
 	);
