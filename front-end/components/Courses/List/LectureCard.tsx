bf4a9d67ac12a0fb2adb25541ceda9a6f42daa99
sonah5009 <sonah5009@gmail.com>
2023-08-03 18:47:24 +0900

    강좌리스트: skeleton screen 기능 추가

--- front-end/components/Courses/List/LectureCard.tsx
+++ front-end/components/Courses/List/LectureCard.tsx
@@ -17,7 +17,34 @@ const LectureCard = ({ course }: { course: ICourseInfo }): JSX.Element => {
 		course.id,
 	);
 
-	if (isLoading) return <div>Loading . . .</div>;
+	if (isLoading)
+		return (
+			<div role="status" className="animate-pulse">
+				<div className="px-1">
+					<div
+						// className="max-w-sm p-4 rounded-md shadow md:p-6 "
+						className="relative h-full px-3 overflow-hidden transition rounded-md"
+					>
+						{/* 그림 */}
+						<div className="w-[300px] h-32 flex items-center justify-center mb-3 bg-gray-300 rounded-md dark:bg-gray-700"></div>
+						{/* 썸네일 외 정보 */}
+						<div className="flex flex-col justify-between h-20 pt-1 pb-3">
+							{/* 제목 */}
+							<div>
+								<div className="h-3 mb-3 bg-gray-200 rounded-full w-44 dark:bg-gray-700"></div>
+								<div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-36"></div>
+							</div>
+							{/* 강사, 명수 */}
+							<div className="flex items-center justify-between">
+								<div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-[20%]"></div>
+								<div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-[15%]"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<span className="sr-only">Loading...</span>
+			</div>
+		);
 
 	const handleClick = (id: number) => {
 		router.push(`/courses/${id}`);
@@ -38,9 +65,9 @@ const LectureCard = ({ course }: { course: ICourseInfo }): JSX.Element => {
 					alt="course thumbnail"
 				/>
 				{/* 썸네일 외 정보 */}
-				<div className="flex flex-col justify-between px-3 pt-2 pb-3 h-30 h-24">
+				<div className="flex flex-col justify-between h-24 px-2 pt-2 pb-3">
 					<div className="font-bold">{course.title}</div>
-					<div className="flex justify-between items-center">
+					<div className="flex items-center justify-between">
 						<div className="text-xs opacity-[0.6] mt-2 overflow-ellipsis">
 							{popularContents?.instructorName}
 						</div>
--- front-end/components/Courses/List/LectureList.tsx
+++ front-end/components/Courses/List/LectureList.tsx
@@ -21,20 +21,53 @@ const LectureList = () => {
 		category2sId,
 		difficulty,
 	});
+	const skeleton_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-	if (isLoading) return <div>Loading . . .</div>;
+	if (!courseList || courseList.length === 0) return null;
 
-	return (
-		<div className="">
-			<div className="grid grid-cols-4 px-6 mb-12 gap-x-5 gap-y-14">
-				{!!courseList?.length ? (
-					courseList?.map((course, index) => (
-						<LectureCard course={course} key={index} />
-					))
-				) : (
-					<div>검색 결과가 없습니다!!!</div>
-				)}
+	// skeleton screen: 로딩 중에 회색화면으로 미리 나오는 거
+	if (isLoading)
+		return (
+			<div
+				role="status"
+				className="grid grid-cols-4 mb-12 px-7 gap-x-5 gap-y-14 animate-pulse"
+			>
+				{skeleton_num.map((_, idx) => (
+					<div key={idx} className="px-1">
+						<div
+							// className="max-w-sm p-4 rounded-md shadow md:p-6 "
+							className="relative h-full px-3 overflow-hidden transition rounded-md"
+						>
+							{/* 그림 */}
+							<div className="w-[300px] h-32 flex items-center justify-center mb-3 bg-gray-300 rounded-md dark:bg-gray-700"></div>
+							{/* 썸네일 외 정보 */}
+							<div className="flex flex-col justify-between h-20 pt-1 pb-3">
+								{/* 제목 */}
+								<div>
+									<div className="h-3 mb-3 bg-gray-200 rounded-full w-44 dark:bg-gray-700"></div>
+									<div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-36"></div>
+								</div>
+								{/* 강사, 명수 */}
+								<div className="flex items-center justify-between">
+									<div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-[20%]"></div>
+									<div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-[15%]"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				))}
+				<span className="sr-only">Loading...</span>
 			</div>
+		);
+	return (
+		<div className="grid grid-cols-4 px-6 mb-12 gap-x-5 gap-y-14">
+			{!!courseList.length ? (
+				courseList.map((course, index) => (
+					<LectureCard course={course} key={index} />
+				))
+			) : (
+				<div>검색 결과가 없습니다!!!</div>
+			)}
 		</div>
 	);
 };
