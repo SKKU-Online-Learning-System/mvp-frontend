e5bc5439422543c028825da6e0bc57b5a255560c
RSuhyeon <fbtngus2789@naver.com>
2023-08-04 18:02:51 +0900

    CourseHeader 컴포넌트 포인트 컬러 통일화

--- front-end/components/Main/CourseHeader.tsx
+++ front-end/components/Main/CourseHeader.tsx
@@ -3,21 +3,11 @@ import React from 'react';
 type PropType = { title: string };
 
 const CourseHeader = ({ title }: PropType) => {
-	const color =
-		title === '인기 컨텐츠'
-			? '#ff0000'
-			: title === '신규 컨텐츠'
-			? '#00ff00'
-			: title === '인공지능 컨텐츠'
-			? '#0000ff'
-			: '#dfdf00';
-
 	return (
 		<div className="w-full text-[2rem] font-bold pb-3 pl-[45px] pt-[60px] border-b-2 border-b-solid border-b-black/[0.2]">
 			<div className="relative">
 				<div
-					style={{ backgroundColor: color }}
-					className={`w-5 h-0.5 absolute left-1`}
+					className={`w-5 h-0.5 absolute left-1 bg-[var(--color-mrgreen-7)]`}
 				></div>
 				{title}
 			</div>
