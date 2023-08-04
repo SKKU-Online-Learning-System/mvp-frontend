19056775c62b9fc9e001fa7ba4ef39255fd28dc8
RSuhyeon <fbtngus2789@naver.com>
2023-08-04 15:55:11 +0900

    Design: Footer 컴포넌트 내 padding과 각 요소 별 margin 증가

--- front-end/components/Footer.tsx
+++ front-end/components/Footer.tsx
@@ -1,55 +1,55 @@
 import React from 'react';
+import Link from 'next/link';
+
 import { DEVICE_BREAKPOINT } from 'constants/breakpoint';
 
 const Footer = () => {
 	return (
-		<footer className="flex justify-center items-center min-w-[desktop] bg-[#323232] text-[#fefefe]">
+		<footer className="flex justify-center items-center min-w-[desktop] bg-[#323232] text-[#fefefe] p-4">
 			<div
 				className="flex justify-between"
 				style={{ width: DEVICE_BREAKPOINT.DESKTOP }}
 			>
 				<div className="m-[34px]">
-					<div className="flex mb-6">
+					<div className="flex mb-8">
 						<img src="/images/main_logo.png" alt="Main Logo Image" />
 						<div className="font-[Gugi] text-[2rem] my-0 ml-[20px] text-[#c7c6c6]">
 							온라인 명륜당
 						</div>
 					</div>
-					<p>성균관대학교 소프트웨어융합대학</p>
-					<p>경기도 수원시 장안구 서부로 2066 성균관대학교 자연과학캠퍼스</p>
+					<p className="mb-1">성균관대학교 소프트웨어융합대학</p>
+					<p className="mb-1">
+						경기도 수원시 장안구 서부로 2066 성균관대학교 자연과학캠퍼스
+					</p>
 					<p>
 						소프트웨어융합대학 Copyrght ⓒ 2022 SUNGKYUNKWAN UNIVERSITY ALL
 						RIGHTS RESERVED.
 					</p>
 				</div>
 				<div className="m-[34px] mt-11">
-					<a
-						href="https://www.skku.edu/skku/etc/pop_email.do"
-						className="text-[#fefefe] no-underline hover:text-[#c2d45e]"
-					>
-						이메일
-					</a>
+					<Link href="https://www.skku.edu/skku/etc/pop_email.do">
+						<span className="text-[#fefefe] no-underline cursor-pointer">
+							이메일
+						</span>
+					</Link>
 					<span className="m-4">|</span>
-					<a
-						href="https://www.skku.edu/skku/etc/pop_email.do"
-						className="text-[#fefefe] no-underline hover:text-[#c2d45e]"
-					>
-						무단수집거부
-					</a>
+					<Link href="https://www.skku.edu/skku/etc/pop_email.do">
+						<span className="text-[#fefefe] no-underline cursor-pointer">
+							무단수집거부
+						</span>
+					</Link>
 					<span className="m-4">|</span>
-					<a
-						href="https://www.skku.edu/skku/etc/private.do"
-						className="text-[#fefefe] no-underline hover:text-[#c2d45e]"
-					>
-						개인정보처리방침
-					</a>
+					<Link href="https://www.skku.edu/skku/etc/private.do">
+						<span className="text-[#fefefe] no-underline cursor-pointer">
+							개인정보처리방침
+						</span>
+					</Link>
 					<span className="m-4">|</span>
-					<a
-						href=""
-						className="text-[#fefefe] no-underline hover:text-[#c2d45e]"
-					>
-						Contact us
-					</a>
+					<Link href="">
+						<span className="text-[#fefefe] no-underline cursor-pointer">
+							Contact us
+						</span>
+					</Link>
 				</div>
 			</div>
 		</footer>
