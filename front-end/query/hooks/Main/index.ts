b869e48508e0436cf740a68b21eef224e19a3cd9
RSuhyeon <fbtngus2789@naver.com>
2023-08-05 18:48:22 +0900

    Next.js SSG SSR 활용으로 인한 query hook 제거

--- front-end/query/hooks/Main/index.ts
+++ front-end/query/hooks/Main/index.ts
@@ -2,19 +2,7 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
 import { QUERY_KEYS } from 'query/hooks/Main/queryKeys';
 import { ILatestLecture } from 'types/MyPage';
-import { MainCourse } from 'types//Main';
-import mainPageAPI from 'apis/Main';
 import myPageAPI from 'apis/MyPage';
-
-export const useRecommendedCoursesFetch = (order: number) => {
-	return useQuery<MainCourse[]>(
-		[QUERY_KEYS.FETCH_RECOMMENDED_COURSE, order],
-		() => mainPageAPI.fetchRecommendedCourse(order),
-		{
-			staleTime: 1000,
-		},
-	);
-};
 
 export const useRecentLecturesFetch = (): UseQueryResult<ILatestLecture[]> => {
 	return useQuery<ILatestLecture[]>([QUERY_KEYS.FETCH_RECENT_LECTURES], () =>
