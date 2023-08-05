c59bb15b39b4a31ed6f97238524e597454d18912
RSuhyeon <fbtngus2789@naver.com>
2023-08-05 16:23:46 +0900

    CourseList 페이지 디자인 통일화 중

--- front-end/components/Courses/List/CourseListLayout.tsx
+++ front-end/components/Courses/List/CourseListLayout.tsx
@@ -1,17 +1,40 @@
 import React, { useState, ReactElement } from 'react';
-import Link from 'next/dist/client/link';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 import menus from 'constants/MyPage/myPageMenus';
+import { ICourseCategory } from 'types/Course';
 
 type PropsType = {
 	children: ReactElement[] | ReactElement<any, string> | string;
+	categories: ICourseCategory[];
 };
 
-const MyPageLayout = ({ children }: PropsType) => {
+const CourseListLayout = ({ children, categories }: PropsType) => {
+	const router = useRouter();
+
 	const [isClicked, setIsClicked] = useState(menus[0].title);
+	const [isClickedCategory, setIsClickedCategory] = useState<boolean[]>([]);
 
 	const handleMenuClick = (event: string) => {
 		setIsClicked(event);
+	};
+
+	const handleCardClick = (clickedIndex: number) => () => {
+		if (!clickedIndex) router.push('/courses');
+
+		const clickedCategory = isClickedCategory.map((value, index) =>
+			clickedIndex === index ? !value : value,
+		);
+
+		setIsClickedCategory(clickedCategory);
+	};
+
+	const handleSubItemClick = (category2sId: number) => () => {
+		router.push({
+			pathname: '/courses',
+			query: { category2sId },
+		});
 	};
 
 	const handleSidebarBtnStyle = (open: boolean) => {
@@ -29,34 +52,64 @@ const MyPageLayout = ({ children }: PropsType) => {
 	return (
 		<div className="w-full h-full flex flex-col my-0 mx-auto font-['Noto Sans KR']">
 			<h2 className="select-none w-full bg-[var(--color-Primary)] p-8 font-['Gugi'] text-2xl text-white border-b-2 border-solid border-[var(--color-Background)]">
-				{'온라인명륜당 > 마이페이지'}
+				{'온라인명륜당 > 강좌 List'}
 			</h2>
 			<div className="flex">
 				<div className="min-h-screen w-1/6 bg-[var(--color-Primary)] min-w-[280px]">
-					<ul className="flex flex-col justify-start h-full p-12 pr-0">
-						{menus.map((menu, idx) => (
-							<li
-								key={idx}
-								className="relative w-full"
-								onClick={() => handleMenuClick(menu.title)}
-							>
-								<Link href={menu.path}>
+					<ul>
+						{categories.map((content, idx) => {
+							const isClicked = isClickedCategory[idx];
+							return (
+								<li key={idx}>
 									<div
-										className={handleSidebarBtnStyle(isClicked === menu.title)}
+										className="flex font-semibold transition text-[#585858] hover:text-[#121212] cursor-pointer p-[0.85rem] bg-[var(--color-Surface)]"
+										onClick={handleCardClick(idx)}
 									>
-										<span>{menu.title}</span>
+										{content.name}
 									</div>
-								</Link>
-							</li>
-						))}
+									{isClicked && (
+										<div>
+											{content.category2s?.map((elem, idx) => (
+												<div
+													className="bg-[var(--color-Surface] hover:bg-[var(--color-Surface)] cursor-pointer font-normal 
+                                                    text-[#595959] border-[0.1px] border-b-[0.5px] border-[#e4e4e4] border-solid p-[0.8rem] pl-[1.5rem]"
+													onClick={handleSubItemClick(elem.id)}
+													key={idx}
+												>
+													{elem.name}
+												</div>
+											))}
+										</div>
+									)}
+								</li>
+							);
+						})}
 					</ul>
 				</div>
-				<div className="w-full tbl:w-[80%] bg-[var(--color-Surface)]">
-					{children}
-				</div>
+				<div className="tbl:w-[80%] bg-[var(--color-Surface)]">{children}</div>
 			</div>
 		</div>
 	);
 };
 
-export default MyPageLayout;
+export default CourseListLayout;
+
+{
+	/* <ul className="flex flex-col justify-start h-full p-12 pr-0">
+    {menus.map((menu, idx) => (
+        <li
+            key={idx}
+            className="relative w-full"
+            onClick={() => handleMenuClick(menu.title)}
+        >
+            <Link href={menu.path}>
+                <div
+                    className={handleSidebarBtnStyle(isClicked === menu.title)}
+                >
+                    <span>{menu.title}</span>
+                </div>
+            </Link>
+        </li>
+    ))}
+</ul> */
+}
--- front-end/pages/courses/index.tsx
+++ front-end/pages/courses/index.tsx
@@ -1,47 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import CourseCategory from '@components/Courses/List/CourseCategory';
+import CourseListLayout from '@components/Courses/List/CourseListLayout';
 import TopSearchbar from '@components/Courses/List/TopSearchbar';
 import LectureList from '@components/Courses/List/LectureList';
+import courseAPI from '../../apis/Courses/courseApi';
+import { ICourseCategory } from 'types/Course';
+
+type PropsType = {
+	categories: ICourseCategory[];
+};
 
 // TODO. 검색을 했을경우 breadCrumb 카테고리가 맞지 않는 문제 해결
-const CoursesListPage = () => {
-	const [menu, setMenu] = useState<string[]>([]);
-
-	const handleClickMenu = (menu: string[]) => {
-		setMenu(menu);
-	};
-
+const CoursesListPage = ({ categories }: PropsType) => {
 	return (
-		<>
-			{/* <div className="min-h-full">
-				<h2 className="select-none w-full bg-[var(--color-green-700)] p-8 font-['Gugi'] text-2xl text-white border-b-2 border-solid border-[var(--color-Background)]">
-					{'온라인명륜당 > 강좌 List'}
-				</h2>
-				<div className="flex min-h-screen">
-					<div className="w-1/6 min-h-full bg-[var(--color-Primary)] min-w-[280px]">
-						<ul className="flex flex-col items-start justify-start h-full p-12 pr-0">
-							<li className="relative w-full">
-								<button id="compose">편성</button>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div> */}
+		<CourseListLayout categories={categories}>
 			<div className="flex max-w-[1400px] p-8 mx-auto font-['Noto Sans KR']">
 				<div className="flex ">
-					<nav className="w-1/6">
-						<CourseCategory handleClickMenu={handleClickMenu} />
-					</nav>
 					<div className="w-full">
-						<div className="px-6 mb-5 "></div>
 						<TopSearchbar />
 						<LectureList />
 					</div>
 				</div>
 			</div>
-		</>
+		</CourseListLayout>
 	);
 };
+
+export async function getStaticProps() {
+	const res = await courseAPI.fetchAllCourseCategories();
+	return {
+		props: {
+			categories: res,
+		},
+	};
+}
 
 export default CoursesListPage;
