1422e8c9471bc314e9ff3fd7e209a4d1591d63c7
RSuhyeon <fbtngus2789@naver.com>
2023-08-05 15:50:32 +0900

    불필요한 주석 제거

--- front-end/query/hooks/CourseList/index.ts
+++ front-end/query/hooks/CourseList/index.ts
@@ -17,11 +17,6 @@ const selectCourseCategories = (data: ICourseCategory[]) => [
 	...data,
 ];
 
-// 모든 강좌 카테고리를 가져옴
-// useQuery 훅을 사용하여 API에서 데이터를 가져옴.
-// 가져온 데이터에 전체보기 옵션을 추가하여,
-// 사용자가 모든 강좌 카테고리를 볼 수 있도록 함
-
 export const useCourseCategoriesFetch = (): UseQueryResult<
 	ICourseCategory[]
 > => {
@@ -34,10 +29,6 @@ export const useCourseCategoriesFetch = (): UseQueryResult<
 	);
 };
 
-// 모든 강좌 리스트를 가져옴
-// useQuery 훅을 사용하여 API에서 데이터를 가져옴.
-// 다양한 매개변수들을 사용하여 API요청을 만듦.
-// 매개변수: category, 난이도, 키워드  포함
 export const useCourseListFetch = (
 	params: ICourseListParams,
 	options?: UseQueryOptions<ICourseInfo[]>,
@@ -63,8 +54,6 @@ export const useCourseListFetch = (
 		options,
 	);
 };
-
-// 모든 인기강좌 정보를 가져옴
 
 export const usePopularCoursesFetch = (
 	courseId: number,
