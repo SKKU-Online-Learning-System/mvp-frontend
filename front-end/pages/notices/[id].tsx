a7162acdcdb5bb41a5d8249e93f72fb9c37cf5ed
RSuhyeon <fbtngus2789@naver.com>
2023-08-08 15:30:37 +0900

    개별 공지사항 페이지 제작

--- front-end/pages/notices/[id].tsx
+++ front-end/pages/notices/[id].tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import type { GetStaticPropsContext } from 'next';
 
-const NoticePage = () => {
-	return <div>Hi</div>;
+import noticeAPI from '../../apis/Notices/noticesAPI';
+import { Notification } from 'types/Notification';
+
+type PropsType = {
+	notice: Notification;
 };
+
+const NoticePage = ({ notice }: PropsType) => {
+	if (!notice) return <div>Failed to retrieve the notice . . .</div>;
+
+	const date = notice.createdAt.split('T')[0];
+
+	return (
+		<div className="max-w-[1200px] flex flex-col items-center justify-start p-10 mx-auto">
+			<div className="w-full flex flex-col justify-center items-center border-solid border-t-[1px] border-b-[1px] border-[#393939] py-4">
+				<div className="mb-3 text-3xl font-semibold">{notice.title}</div>
+				<div className="flex">
+					<span>No.{notice.id}</span>
+					<span className="mx-3">|</span>
+					<span>온라인명륜당 운영진</span>
+					<span className="mx-3">|</span>
+					<span>{date}</span>
+					<span className="mx-3">|</span>
+					<span>조회수: {notice.views}</span>
+				</div>
+			</div>
+			<p className="p-10">{notice.content}</p>
+		</div>
+	);
+};
+
+export async function getStaticProps({ params }: GetStaticPropsContext) {
+	if (!params || !params.id) return { props: {} };
+
+	const id = +params.id;
+	const notice = await noticeAPI.fetchNotice(id);
+
+	return { props: { notice } };
+}
+
+export async function getStaticPaths() {
+	return { paths: [], fallback: 'blocking' };
+}
 
 export default NoticePage;
