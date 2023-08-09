e8caaac4f0a46807cda044a341867988a50edad8
RSuhyeon <fbtngus2789@naver.com>
2023-08-09 21:15:52 +0900

    Admin 페이지 강좌 관리 섹션 내 강좌 운영 상태 업데이트 UI 변경

--- front-end/components/Admin/contents-manage/CardHeader.tsx
+++ front-end/components/Admin/contents-manage/CardHeader.tsx
@@ -16,11 +16,12 @@ const CardHeader = ({
 	return (
 		<div className="flex items-center justify-between w-full bg-[var(--color-onSurface-100)] px-[5%] py-[2.5%] bg-red">
 			<div className="flex flex-col items-start justify-center">
-				<div>
-					<span>현재 운영상태: </span>
-					<span>{operation ? '운영 중' : '미운영 중'}</span>
-				</div>
-				<button onClick={onChangeOperation}>운영상태 변경</button>
+				<button
+					onClick={onChangeOperation}
+					className="text-xl font-semibold rounded-lg bg-[#b3df8c] py-2 px-4 shadow-lg transition hover:bg-[#b9c7ad]"
+				>
+					{operation ? '운영 中' : '미운영'}
+				</button>
 			</div>
 			<h3 className="text-[28px] font-bold">{title}</h3>
 			{instructor ? <span>{instructor} 교수</span> : ''}
