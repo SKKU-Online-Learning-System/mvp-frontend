841ad3232fe095b45b0f0056ea9f4e390e5b5bcb
RSuhyeon <fbtngus2789@naver.com>
2023-08-10 15:43:16 +0900

    메인페이지 최상단 이동 버튼 hovering 시 effect 변경

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -48,7 +48,7 @@ const MainPage = ({ recommendedContents }: PropsType) => {
 				onClick={() => {
 					window.scrollTo(0, 0);
 				}}
-				className="fixed shadow-lg bottom-[50px] right-[50px] w-[50px] h-[50px] text-2xl text-white bg-black rounded-full flex justify-center items-center transition-all hover:text-3xl"
+				className="fixed shadow-lg bottom-[50px] right-[50px] w-[50px] h-[50px] text-2xl text-white bg-black rounded-full flex justify-center items-center transition-all hover:bg-[#393939] hover:text-[28px]"
 			>
 				<FontAwesomeIcon icon={faAngleUp} />
 			</button>
