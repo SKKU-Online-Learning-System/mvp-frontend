a4261a0819137ebd2d545f9478731d5aeb878be9
RSuhyeon <fbtngus2789@naver.com>
2023-08-10 22:29:51 +0900

    Admin 페이지 강좌 관리 섹션 운영 여부 변경 버튼 컴포넌트화 진행

--- front-end/components/Admin/contents-manage/CardHeader.tsx
+++ front-end/components/Admin/contents-manage/CardHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import OperationBtn from './OperationBtn';
+
 type PropType = {
 	title: string;
 	instructor: string;
@@ -15,14 +17,7 @@ const CardHeader = ({
 }: PropType) => {
 	return (
 		<div className="flex items-center justify-between w-full bg-[var(--color-onSurface-100)] px-[5%] py-[2.5%] bg-red">
-			<div className="flex flex-col items-start justify-center">
-				<button
-					onClick={onChangeOperation}
-					className="text-xl font-semibold rounded-lg bg-[#b3df8c] py-2 px-4 shadow-lg transition hover:bg-[#b9c7ad]"
-				>
-					{operation ? '운영 中' : '미운영'}
-				</button>
-			</div>
+			<OperationBtn oper={operation} onChangeOper={onChangeOperation} />
 			<h3 className="text-[28px] font-bold">{title}</h3>
 			{instructor ? <span>{instructor} 교수</span> : ''}
 		</div>
