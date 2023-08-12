f4f2eca5626a74f5379793cbbe52fdb38afe17af
RSuhyeon <fbtngus2789@naver.com>
2023-08-12 20:13:35 +0900

    CourseHeader 컴포넌트 구조 조정

--- front-end/components/Courses/Details/CourseHeader/CourseHeader.tsx
+++ front-end/components/Courses/Details/CourseHeader/CourseHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/dist/client/image';
 
 import MetaDataSection from './MetaDataSection';
+import RegisterButton from './RegisterButton';
 import { ICourseDetail } from 'types/Course';
 
 type PropsType = {
@@ -16,8 +17,8 @@ const CourseHeader = ({
 	onOpenLoginModal,
 }: PropsType) => {
 	return (
-		<section className="flex h-[25rem] bg-[var(--color-Primary)] px-10 justify-center">
-			<div className="my-auto">
+		<section className="w-full flex h-[25rem] bg-[var(--color-Primary)] px-10 justify-center">
+			<section className="my-auto">
 				<Image
 					src={courseDetail.thumbnail}
 					alt="course thumbnail"
@@ -25,12 +26,15 @@ const CourseHeader = ({
 					height={270}
 					className="rounded-md"
 				/>
-			</div>
-			<MetaDataSection
-				courseId={courseId}
-				courseDetail={courseDetail}
-				onOpenLoginModal={onOpenLoginModal}
-			/>
+			</section>
+			<section className="w-[40%] flex flex-col px-12 my-auto text-[var(--color-onPrimary)]">
+				<MetaDataSection courseDetail={courseDetail} />
+				<RegisterButton
+					courseId={courseId}
+					courseDetail={courseDetail}
+					onOpenLoginModal={onOpenLoginModal}
+				/>
+			</section>
 		</section>
 	);
 };
--- front-end/components/Courses/Details/CourseHeader/MetaDataSection.tsx
+++ front-end/components/Courses/Details/CourseHeader/MetaDataSection.tsx
@@ -2,32 +2,20 @@ import React from 'react';
 
 import MetaDataHeader from './MetaDataHeader';
 import CourseHashtags from './CourseHashtags';
-import RegisterButton from './RegisterButton';
 import { ICourseDetail } from 'types/Course';
 
 type PropsType = {
-	courseId: number;
 	courseDetail: ICourseDetail;
-	onOpenLoginModal: () => void;
 };
 
-const MetaDataSection = ({
-	courseId,
-	courseDetail,
-	onOpenLoginModal,
-}: PropsType) => {
+const MetaDataSection = ({ courseDetail }: PropsType) => {
 	const courseDescription = courseDetail.description;
 
 	return (
-		<section className="w-[40%] my-auto text-[var(--color-onPrimary)] px-[50px]">
+		<section>
 			<MetaDataHeader courseDetail={courseDetail} />
 			<article className="m-[5px] mb-8">{courseDescription}</article>
 			<CourseHashtags courseDetail={courseDetail} />
-			<RegisterButton
-				courseId={courseId}
-				courseDetail={courseDetail}
-				onOpenLoginModal={onOpenLoginModal}
-			/>
 		</section>
 	);
 };
