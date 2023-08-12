5eb806ec9b40b5cb48c0b6f2a32e65fcebb444b9
RSuhyeon <fbtngus2789@naver.com>
2023-08-12 20:04:26 +0900

    QnA 컴포넌트 코드 정리

--- front-end/components/Courses/Details/QnA.tsx
+++ front-end/components/Courses/Details/QnA.tsx
@@ -1,41 +1,38 @@
 import React from 'react';
-import { AiFillCaretRight } from 'react-icons/ai';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { AiFillCaretRight } from 'react-icons/ai';
 
 import QnAItem from './QnAItem';
 import { IQna } from 'types/Course';
 
 type PropsType = {
 	courseId: number;
-	qna: IQna[];
+	qna: IQna[] | undefined;
 };
 
 const QnA = ({ courseId, qna }: PropsType) => {
-	const router = useRouter();
-	const recentQna = qna.slice(0, 3);
+	if (!qna) return <div>There is no Q&A data . . .</div>;
 
-	const handleClick = () => {
-		router.push(`/questions/course/${courseId}`);
-	};
+	const recentQna = qna.slice(0, 3);
+	const qnaCnt = qna.length;
 
 	return (
-		<div className="w-[80%] m-auto p-[25px] font-[var(--font-NotoSans)]">
+		<section className="w-[1080px] mx-auto p-6 font-[var(--font-NotoSans)] mb-8">
 			<header className="m-0 mb-4 ">
 				<div className="text-[0.5rem] text-[#c2c1c1] font-bold">
 					Recent Questions
 				</div>
 				<div className="flex">
-					<h2 className="pr-[18px] font-semibold text-2xl">최근 한 질문</h2>
+					<h3 className="pr-[18px] font-semibold text-3xl">최근 한 질문</h3>
 					<Link href={`/questions/course/${courseId}`}>
 						<div className="flex text-xs hover:opacity-90 font-medium my-auto text-[var(--color-onPrimary)] bg-[var(--color-Primary)] rounded-md py-1 px-2 cursor-pointer">
-							{recentQna.length > 0 ? '더보기' : '질문하기'}
+							{qnaCnt > 0 ? '더보기' : '질문하기'}
 							<AiFillCaretRight className="my-auto" />
 						</div>
 					</Link>
 				</div>
 			</header>
-			{recentQna.length === 0 && (
+			{qnaCnt === 0 && (
 				<div className="flex items-center">
 					<div className="py-0 px-[18px]">첫 질문의 주인공이 되어보세요</div>
 				</div>
@@ -49,7 +46,7 @@ const QnA = ({ courseId, qna }: PropsType) => {
 					/>
 				);
 			})}
-		</div>
+		</section>
 	);
 };
 
