6b9b8f319e56113baa35f2cffc80f287f2294ddd
RSuhyeon <fbtngus2789@naver.com>
2023-08-14 18:56:00 +0900

    마이페이지 SSG 도입

--- front-end/pages/my-page/[category].tsx
+++ front-end/pages/my-page/[category].tsx
@@ -1,32 +1,29 @@
 import React from 'react';
-import { useRouter } from 'next/router';
 
 import Learning from '@components/MyPage/Learning/Learning';
 import MyPageLayout from '@components/MyPage/MyPageLayout';
 import History from '@components/MyPage/History/History';
 import Completed from '@components/MyPage/Completed';
 import Bookmark from '@components/MyPage/Bookmark';
+import { GetServerSidePropsContext } from 'next';
 import { MYPAGE_PATH } from 'constants/MyPage';
 import MyQnA from '@components/MyPage/MyQna';
 
-const MyPage = () => {
-	const router = useRouter();
-	const { category } = router.query as { category: string };
+type PropsType = {
+	category: string;
+};
 
+const MyPage = ({ category }: PropsType) => {
 	const routeComponent = (category: string) => {
 		switch (category) {
-			// 최근 시청 강의
 			case MYPAGE_PATH.HISTORY:
 				return <History />;
 			case MYPAGE_PATH.BOOKMARK:
 				return <Bookmark />;
-			// 수강 완료 강좌
 			case MYPAGE_PATH.COMPLETED:
 				return <Completed />;
-			// 수강 중인 강좌
 			case MYPAGE_PATH.LEARNING:
 				return <Learning />;
-			// 내 질문/답변
 			case MYPAGE_PATH.MY_QNA:
 				return <MyQnA />;
 			default:
@@ -36,5 +33,14 @@ const MyPage = () => {
 
 	return <MyPageLayout>{routeComponent(category)}</MyPageLayout>;
 };
+
+export async function getStaticProps({ params }: GetServerSidePropsContext) {
+	const category = params!.category;
+	return { props: { category } };
+}
+
+export async function getStaticPaths() {
+	return { paths: [], fallback: 'blocking' };
+}
 
 export default MyPage;
