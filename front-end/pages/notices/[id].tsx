300f88980269053d9b9c2be3aeb329851edd3cbf
RSuhyeon <fbtngus2789@naver.com>
2023-08-17 15:57:15 +0900

    공지사항 페이지 Layout 적용

--- front-end/pages/notices/[id].tsx
+++ front-end/pages/notices/[id].tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import type { GetStaticPropsContext } from 'next';
 
+import NoticeMetaDataBar from '@components/Notices/NoticeMetaDataBar';
+import NoticesLayout from '@components/Notices/NoticesLayout';
 import noticeAPI from '../../apis/Notices/noticesAPI';
 import { Notification } from 'types/Notification';
-import NoticeMetaDataBar from '@components/Notices/NoticeMetaDataBar';
 
 type PropsType = {
 	notice: Notification;
@@ -11,13 +12,15 @@ type PropsType = {
 
 const NoticePage = ({ notice }: PropsType) => {
 	return (
-		<div className="max-w-[1200px] flex flex-col items-center justify-start p-10 mx-auto">
-			<div className="w-full flex flex-col justify-center items-center border-solid border-t-[1px] border-b-[1px] border-[#393939] py-4">
-				<div className="mb-3 text-3xl font-semibold">{notice.title}</div>
-				<NoticeMetaDataBar notice={notice} />
+		<NoticesLayout>
+			<div className="max-w-[1200px] flex flex-col items-center justify-start p-10 mx-auto">
+				<div className="w-full flex flex-col justify-center items-center border-solid border-t-[1px] border-b-[1px] border-[#393939] py-4">
+					<div className="mb-3 text-3xl font-semibold">{notice.title}</div>
+					<NoticeMetaDataBar notice={notice} />
+				</div>
+				<p className="p-10">{notice.content}</p>
 			</div>
-			<p className="p-10">{notice.content}</p>
-		</div>
+		</NoticesLayout>
 	);
 };
 
--- front-end/pages/notices/index.tsx
+++ front-end/pages/notices/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Notification } from 'types/Notification';
 import noticesAPI from '../../apis/Notices/noticesAPI';
 import NewIndicator from '@components/Notices/NewIndicator';
+import NoticesLayout from '@components/Notices/NoticesLayout';
 import NoticeMetaDataBar from '@components/Notices/NoticeMetaDataBar';
 
 type PropsType = {
@@ -12,46 +13,42 @@ type PropsType = {
 
 const NotificationPage = ({ notices }: PropsType) => {
 	return (
-		<div className="flex items-center justify-center">
-			<div className="flex flex-col items-center justify-start w-[768px] h-full py-12">
-				<div className="w-full px-4 py-2">
-					<h1 className="relative mt-2 mb-4 text-4xl font-semibold ">
-						<div className="w-5 h-0.5 absolute left-1 bg-[var(--color-mrgreen-7)] -top-2"></div>
-						공지사항
-					</h1>
+		<NoticesLayout>
+			<div className="flex items-center justify-center w-full">
+				<div className="flex flex-col items-center justify-start w-[768px] h-full py-12">
+					<ul className="w-full border-solid border-t-2 border-[#393939]">
+						{notices.map((notice, idx) => {
+							const createdAt = notice.createdAt.split('T')[0];
+							const todayTime = new Date().getTime();
+							const createdTime = new Date(
+								+createdAt.split('-')[0],
+								+createdAt.split('-')[1],
+								+createdAt.split('-')[2],
+							).getTime();
+
+							const isNew = todayTime - 1000 * 60 * 60 * 24 * 7 <= createdTime;
+
+							return (
+								<li
+									className="border-solid border-b-[1px] border-[#aeaeae] py-3 px-6 w-full"
+									key={idx}
+								>
+									<div className="flex items-center justify-start mb-2">
+										{isNew ? <NewIndicator /> : null}
+										<Link href={`/notices/${notice.id}`}>
+											<h4 className="text-lg font-semibold cursor-pointer">
+												{notice.title}
+											</h4>
+										</Link>
+									</div>
+									<NoticeMetaDataBar notice={notice} />
+								</li>
+							);
+						})}
+					</ul>
 				</div>
-				<ul className="w-full border-solid border-t-2 border-[#393939]">
-					{notices.map((notice, idx) => {
-						const createdAt = notice.createdAt.split('T')[0];
-						const todayTime = new Date().getTime();
-						const createdTime = new Date(
-							+createdAt.split('-')[0],
-							+createdAt.split('-')[1],
-							+createdAt.split('-')[2],
-						).getTime();
-
-						const isNew = todayTime - 1000 * 60 * 60 * 24 * 7 <= createdTime;
-
-						return (
-							<li
-								className="border-solid border-b-[1px] border-[#aeaeae] py-3 px-6 w-full"
-								key={idx}
-							>
-								<div className="flex items-center justify-start mb-2">
-									{isNew ? <NewIndicator /> : null}
-									<Link href={`/notices/${notice.id}`}>
-										<h4 className="text-lg font-semibold cursor-pointer">
-											{notice.title}
-										</h4>
-									</Link>
-								</div>
-								<NoticeMetaDataBar notice={notice} />
-							</li>
-						);
-					})}
-				</ul>
 			</div>
-		</div>
+		</NoticesLayout>
 	);
 };
 
