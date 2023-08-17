674d860f11b363caeebfddd5ce87e69d2425fcce
RSuhyeon <fbtngus2789@naver.com>
2023-08-17 17:41:12 +0900

    Admin 권한을 가진 유저만 UpperHeader 컴포넌트에 Admin 버튼 뜨도록 설정

--- front-end/components/Header/UpperHeader.tsx
+++ front-end/components/Header/UpperHeader.tsx
@@ -3,9 +3,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
-import { selectIsLoggined } from '../../store/feature/common/commonSelector';
+import {
+	selectIsLoggined,
+	selectUserType,
+} from '../../store/feature/common/commonSelector';
 import { commonActions } from 'store/feature/common/commonSlice';
-import { userLoginAuthState } from '../../constants/commonState';
+import { userLoginAuthState, userState } from '../../constants/commonState';
 import axiosInstance from '../../apis/index';
 
 type PropsType = {
@@ -16,15 +19,13 @@ type PropsType = {
 const upperHeaderStyle =
 	' text-[var(--color-onBackground-300)] mx-4 text-sm font-semibold no-underline cursor-pointer transition-colors duration-150 hover:text-[var(--color-green-300)]';
 
-// const menuData = [
-// 	{ id: 1, name: 'Admin', path: '/admin' },
-// 	{ id: 2, name: '마이페이지', path: '/my-page/profile' },
-// ];
-
 const UpperHeader = ({ onOpenLoginModal, onOpenSignUpModal }: PropsType) => {
 	const router = useRouter();
+
 	const dispatch = useDispatch();
+
 	const isLoggined = useSelector(selectIsLoggined);
+	const userType = useSelector(selectUserType);
 
 	const handleLogout = () => {
 		axiosInstance.get('/auth/logout').then(() => {
@@ -33,55 +34,33 @@ const UpperHeader = ({ onOpenLoginModal, onOpenSignUpModal }: PropsType) => {
 		});
 	};
 
-	const handleClickAdminBtn = () => {
-		router.push(`/admin`);
-	};
-
 	return (
 		<div className="fixed top-0 z-10 flex items-center justify-end w-screen h-8 px-10 mr-8 bg-white ">
-			<a className={upperHeaderStyle}>ENG</a>
 			{!!isLoggined &&
 				(isLoggined === userLoginAuthState.LOGGINED ? (
 					<div>
-						<a href="" className={upperHeaderStyle} onClick={handleLogout}>
-							로그아웃
-						</a>
 						<Link href="/my-page/profile">
 							<span className={upperHeaderStyle}>마이페이지</span>
 						</Link>
-						<a className={upperHeaderStyle} onClick={handleClickAdminBtn}>
-							Admin
-						</a>
+						<button className={upperHeaderStyle} onClick={handleLogout}>
+							로그아웃
+						</button>
+						{userType <= userState.MANAGER ? (
+							<Link href="/admin">
+								<span className={upperHeaderStyle}>Admin</span>
+							</Link>
+						) : null}
 					</div>
 				) : (
 					<div>
-						<a className={upperHeaderStyle} onClick={onOpenLoginModal}>
+						<button className={upperHeaderStyle} onClick={onOpenLoginModal}>
 							로그인
-						</a>
-						<a className={upperHeaderStyle} onClick={onOpenSignUpModal}>
+						</button>
+						<button className={upperHeaderStyle} onClick={onOpenSignUpModal}>
 							회원가입
-						</a>
+						</button>
 					</div>
 				))}
-			{/* <div>
-        <ul className="flex p-0 m-0">
-            {menuData.map((menu) => {
-                isLoggined !== userLoginAuthState.LOGGINED &&
-                menu.name === '마이페이지' ? (
-                    ''
-                ) : (
-                    <li key={menu.id} className="mx-4 my-0 list-none">
-                        <a
-                            href={menu.path}
-                            className={upperHeaderStyle}
-                        >
-                            {menu.name}
-                        </a>
-                    </li>
-                );
-            })}
-        </ul>
-    </div> */}
 		</div>
 	);
 };
