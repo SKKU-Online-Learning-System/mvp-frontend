ba8d37f4417eecc79904eea54770dfb86266d500
RSuhyeon <fbtngus2789@naver.com>
2023-10-30 16:00:04 +0900

    User Ranking section에 렌더링 할 UI 개선

--- front-end/components/Admin/user-ranking/UserRanking.tsx
+++ front-end/components/Admin/user-ranking/UserRanking.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Image from 'next/image';
 
 const UserRanking = () => {
-	return <div className="w-full">User Ranking</div>;
+	return (
+		<div className="w-full">
+			<div className="flex flex-col items-center justify-center w-full h-full bg-white">
+				<div className="shadow-xl text-white bg-[var(--color-mrgreen-5)] rounded-lg text-2xl py-4 px-6 mb-10">
+					User Ranking Section은 아직 활성화되지 않았습니다.
+				</div>
+				<Image
+					className="opacity-25"
+					src={'/images/confucian.jpeg'}
+					width={500}
+					height={500}
+					alt="Maple img"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default UserRanking;
