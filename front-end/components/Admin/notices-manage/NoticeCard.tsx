36d77c322e80d2c149629fb91c463cee06d7cb15
RSuhyeon <fbtngus2789@naver.com>
2023-11-02 10:54:44 +0900

    Admin 페이지 기존 공지사항 관리 섹션 구성하던 컴포넌트 별도 파일로 컴포넌트화 진행

--- front-end/components/Admin/notices-manage/NoticeCard.tsx
+++ front-end/components/Admin/notices-manage/NoticeCard.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 
 import { Notification } from 'types/Notification';
-import noticesAPI from '../../../apis/Notices/noticesAPI';
-import NewIndicator from '@components/Notices/NewIndicator';
+import NoticeCardHeader from './NoticeCardHeader';
+import NoticeCardBody from './NoticeCardBody';
 
 type PropsType = {
 	notice: Notification;
@@ -22,62 +19,23 @@ const NoticeCard = ({
 	createdAt,
 	onNoticeChangeClick,
 }: PropsType) => {
-	const router = useRouter();
+	const [isClicked, setIsClicked] = useState(false);
 
-	const onTitleClick = async () => {
-		const title = notice.title;
-		const content = notice.content;
-		//Todo: 카드 공간 아래로 slide 되면서 content 확인할 수 있도록 구현
-		console.log(title, content);
-	};
-
-	const onDeleteClick = async (id: number) => {
-		if (confirm('해당 공지사항을 정말 삭제하시겠습니까?')) {
-			try {
-				await noticesAPI.deleteNotice(id);
-			} catch (err: any) {
-				alert('공지 삭제 과정에서 알 수 없는 에러가 발생했습니다.');
-			}
-			router.reload();
-		}
+	const onCardClick = () => {
+		setIsClicked(!isClicked);
 	};
 
 	return (
-		<li
-			className="w-full px-6 py-3 bg-[var(--color-Surface)] rounded-xl mb-4"
-			key={idx}
-		>
-			<div className="flex justify-between">
-				<div>
-					<div className="flex items-center justify-start mb-3">
-						{isNew ? <NewIndicator /> : null}
-						<h4
-							onClick={() => onTitleClick()}
-							className="text-lg font-semibold cursor-pointer"
-						>
-							{notice.title}
-							<FontAwesomeIcon
-								icon={faAngleDown}
-								className={`ml-2 transition`}
-							/>
-						</h4>
-					</div>
-					<div className="text-sm">
-						<span>No.{notice.id}</span>
-						<span className="mx-3">|</span>
-						<span>온라인명륜당 운영진</span>
-						<span className="mx-3">|</span>
-						<span>{createdAt}</span>
-						<span className="mx-3">|</span>
-						<span>조회수: {notice.views}</span>
-					</div>
-				</div>
-				<div className="text-sm">
-					<button onClick={() => onNoticeChangeClick(notice.id)}>수정</button>
-					<span className="mx-2">|</span>
-					<button onClick={() => onDeleteClick(notice.id)}>삭제</button>
-				</div>
-			</div>
+		<li className="w-full mb-8 rounded-md shadow-lg" key={idx}>
+			<NoticeCardHeader
+				notice={notice}
+				isNew={isNew}
+				isClicked={isClicked}
+				createdAt={createdAt}
+				onNoticeChangeClick={onNoticeChangeClick}
+				onCardClick={onCardClick}
+			/>
+			{isClicked ? <NoticeCardBody notice={notice} /> : null}
 		</li>
 	);
 };
