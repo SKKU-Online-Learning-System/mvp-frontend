14e7b56685db118f91db1a94c308559decff67b6
RSuhyeon <fbtngus2789@naver.com>
2023-11-07 16:34:38 +0900

    -

--- front-end/components/Admin/user-ranking/UserRanking.tsx
+++ front-end/components/Admin/user-ranking/UserRanking.tsx
@@ -5,7 +5,7 @@ import NoContent from '@components/NoContent';
 const UserRanking = () => {
 	return (
 		<div className="w-full">
-			<NoContent text="User Ranking Section은 아직 활성화되지 않았습니다." />
+			<NoContent text="" />
 		</div>
 	);
 };
