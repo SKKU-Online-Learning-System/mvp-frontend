a29a805ca8923e40c456a844b80734195f232ce4
sonah5009 <sonah5009@gmail.com>
2023-12-21 15:19:35 +0900

    외부 강좌 추천 페이지 폴더 및 파일 추가

---
+++ front-end/pages/recommend/index.tsx
@@ -0,0 +1,12 @@
+// 외부 컨텐츠 추천 페이지
+// TODO: PropsType 설정
+
+import React from 'react';
+type PropsType = {
+	recommend: null;
+};
+const RecommendPage = ({ recommend }: PropsType): JSX.Element => {
+	return <div>RecommendPage</div>;
+};
+
+export default RecommendPage;
