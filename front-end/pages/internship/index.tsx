11551b023e1ed56f5925536bd58cc6fa2c6ba96a
sonah5009 <sonah5009@gmail.com>
2024-01-12 11:39:35 +0900

    internship pages 수정

--- front-end/pages/internship/index.tsx
+++ front-end/pages/internship/index.tsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import Image from 'next/image';
 
-import { Company } from 'types/Internship';
-
-type PropsType = {
-	company: Company;
+const IntershipPage = (): JSX.Element => {
+	return <div>IntershipPage</div>;
 };
 
-const InternshipCard = ({ company }: PropsType): JSX.Element => {
-	return (
-		<div>
-			{/* <Image
-				width={280}
-				height={140}
-				alt="company image"
-				src={company.company_img}
-			/> */}
-			<div>
-				{/* <span>{company.name}</span> */}
-				<span>영상개수</span>
-			</div>
-		</div>
-	);
-};
-
-export default InternshipCard;
+export default IntershipPage;
